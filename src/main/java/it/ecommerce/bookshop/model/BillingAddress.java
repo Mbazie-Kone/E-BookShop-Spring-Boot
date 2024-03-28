130afa47ead5f1ee85ac9f608ee2973d0b7813a8
M'bazie Kone <mbazie89@gmail.com>
2024-03-29 00:46:19 +0100

    added more information in model package

---
+++ src/main/java/it/ecommerce/bookshop/model/BillingAddress.java
@@ -0,0 +1,34 @@
+package it.ecommerce.bookshop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "billing_addresses")
+public class BillingAddress {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "address_name")
+	private String addressName;
+	
+	@Column(name = "street_1")
+	private String street1;
+	
+	@Column(name = "street_2")
+	private String street2;
+	
+	private String city;
+	private String country;
+	private String state;
+	
+	@Column(name = "zip_code")
+	private String zipCode;
+	
+}
--- src/main/java/it/ecommerce/bookshop/model/Book.java
+++ src/main/java/it/ecommerce/bookshop/model/Book.java
@@ -1,15 +1,46 @@
 package it.ecommerce.bookshop.model;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "books")
 public class Book {
 	
-	private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
 	private String title;
 	private String author;
 	private String publisher;
-	private String publicationDate;
+	
+	@Column(name = "publication_date")
+	private Date publicationDate;
+	
 	private String language;
 	private String category;
+	
+	@Column(name = "number_of_pages")
+	private int numberOfPages;
+	
+	private String format;
+	private int isbn;
+	
+	@Column(name = "shipping_weight")
+	private double shippingWeight;
+	
+	@Column(name = "list_price")
+	private double listPrice;
+	
+	@Column(name = "our_price")
+	private double ourPrice;
+	
+	private boolean active = true;
 }
