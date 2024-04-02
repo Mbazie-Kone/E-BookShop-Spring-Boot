5eb9f9cc5a50390aa43fcafda6bb00458a4879bd
M'bazie Kone <mbazie89@gmail.com>
2024-04-02 06:24:49 +0200

    updated sql file

--- databases/e_bookshop.sql
+++ databases/e_bookshop.sql
@@ -40,6 +40,10 @@
 
 -- L’esportazione dei dati non era selezionata.
 
+-- L’esportazione dei dati non era selezionata.
+
+-- L’esportazione dei dati non era selezionata.
+
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
