dd65f9066ac2d1cc3bcb2ea1eb940c8ec9aebfe4
M'bazie Kone <mbazie89@gmail.com>
2024-04-13 01:51:43 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/EBookShopApplication.java
+++ src/main/java/it/ecommerce/bookshop/EBookShopApplication.java
@@ -1,14 +1,19 @@
 package it.ecommerce.bookshop;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "it.ecommerce.bookshop.repository")
-public class EBookShopApplication {
+public class EBookShopApplication implements CommandLineRunner {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(EBookShopApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
 	}
 }
