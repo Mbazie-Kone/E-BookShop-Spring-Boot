b00807985c4e0078abb93160a809082a2e1b494a
M'bazie Kone <mbazie89@gmail.com>
2024-04-25 02:27:34 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/EBookShopApplication.java
+++ src/main/java/it/ecommerce/bookshop/EBookShopApplication.java
@@ -1,20 +1,14 @@
 package it.ecommerce.bookshop;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "it.ecommerce.bookshop")
-public class EBookShopApplication implements CommandLineRunner {
+public class EBookShopApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EBookShopApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		
 	}
 }
