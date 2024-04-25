159deb322e9d3cb378516b472cc1736726517beb
M'bazie Kone <mbazie89@gmail.com>
2024-04-25 22:39:39 +0200

    I added the CartItem entity

---
+++ src/main/java/it/ecommerce/bookshop/model/CartItem.java
@@ -0,0 +1,18 @@
+package it.ecommerce.bookshop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cart_item_id")
+	private Long id;
+}
