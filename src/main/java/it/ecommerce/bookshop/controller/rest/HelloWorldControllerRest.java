829c45517997bdd7ae8b7c8e3685fe140322c620
M'bazie Kone <mbazie89@gmail.com>
2024-04-25 22:08:20 +0200

    Updates

---
+++ src/main/java/it/ecommerce/bookshop/controller/rest/HelloWorldControllerRest.java
@@ -0,0 +1,10 @@
+package it.ecommerce.bookshop.controller.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldControllerRest {
+	
+	
+	
+}
