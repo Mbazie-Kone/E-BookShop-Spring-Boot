0d7a0896513b9c6b55deb9570601200cbf675c01
M'bazie Kone <mbazie89@gmail.com>
2024-04-28 01:38:50 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/app-routing.module.ts
+++ front-end/e-bookshop-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path:'', component: IndexComponent}
+  {path:'', component: IndexComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'login', component: LoginComponent},
 ];
 
 @NgModule({
