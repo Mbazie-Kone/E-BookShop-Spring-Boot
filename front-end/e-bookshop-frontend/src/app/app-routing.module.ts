f5e6880d6029bcb5922e1cba14af441872074ccc
M'bazie Kone <mbazie89@gmail.com>
2024-04-28 21:01:27 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/app-routing.module.ts
+++ front-end/e-bookshop-frontend/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { IndexComponent } from './index/index.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { SingleContactsComponent } from './single-contacts/single-contacts.component';
 
 const routes: Routes = [
   {path:'', component: IndexComponent},
   {path:'dashboard', component: DashboardComponent},
   {path:'login', component: LoginComponent},
-  {path:'contacts', component: ContactsComponent},
-  {path:'contacts/:id', component: ContactsComponent},
+  {path:'contacts', component: ContactsComponent, children: [{path:':id', component: SingleContactsComponent}]},
+  // {path:'contacts/:id', component: ContactsComponent},
 ];
 
 @NgModule({
