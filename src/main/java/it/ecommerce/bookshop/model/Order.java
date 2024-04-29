6fdd58b907d50643121b9fc6720462d5931f4229
M'bazie Kone <mbazie89@gmail.com>
2024-04-29 02:50:47 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/Order.java
+++ src/main/java/it/ecommerce/bookshop/model/Order.java
@@ -1,11 +1,16 @@
 package it.ecommerce.bookshop.model;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 @Table(name = "orders")
@@ -16,5 +21,24 @@ public class Order {
 	@Column(name = "order_id")
 	private Long id;
 	
+	@Column(name = "order_traking_number")
+	private String orderTrakingNumber;
 	
+	@Column(name = "total_qty")
+	private int totalQty;
+	
+	@Column(name = "total_price")
+	private BigDecimal totalPrice;
+	
+	private String status;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "date_created")
+	private Date dateCreated;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "last_updated")
+	private Date lastUpdated;
+	
+	private Customer customer;
 }
