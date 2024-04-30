cc564eb22464dc42b233e8be2e32f6719525c6c0
M'bazie Kone <mbazie89@gmail.com>
2024-04-30 20:50:28 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/app-routing.module.ts
+++ front-end/e-bookshop-frontend/src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IndexComponent } from './index/index.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { SingleContactsComponent } from './single-contacts/single-contacts.component';
 
 const routes: Routes = [
-  {path:'', component: IndexComponent},
-  {path:'dashboard', component: DashboardComponent},
-  {path:'login', component: LoginComponent},
-  {path:'contacts', component: ContactsComponent, children: [{path:':id', component: SingleContactsComponent}]},
-  // {path:'contacts/:id', component: ContactsComponent},
 ];
 
 @NgModule({
--- front-end/e-bookshop-frontend/src/app/app.module.ts
+++ front-end/e-bookshop-frontend/src/app/app.module.ts
@@ -3,23 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {MatButtonModule} from '@angular/material/button';
-import { ContactsComponent } from './contacts/contacts.component';
-import { SingleContactsComponent } from './single-contacts/single-contacts.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent,
-    LoginComponent,
-    DashboardComponent,
-    ContactsComponent,
-    SingleContactsComponent,
+
   ],
   imports: [
     BrowserModule,
