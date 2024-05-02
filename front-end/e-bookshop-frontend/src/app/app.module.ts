98d6ee74fefd6e9489b64ef03a98b0a7f8d25c08
M'bazie Kone <mbazie89@gmail.com>
2024-05-02 19:49:59 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/app.module.ts
+++ front-end/e-bookshop-frontend/src/app/app.module.ts
@@ -4,21 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
     HttpClientModule,
+    MatToolbarModule,
   ],
   providers: [
     provideAnimationsAsync()
