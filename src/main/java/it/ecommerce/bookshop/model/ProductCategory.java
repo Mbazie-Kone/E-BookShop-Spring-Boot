fbd448054436b459225f44c287a6aa88a0781e8e
M'bazie Kone <mbazie89@gmail.com>
2024-05-03 23:13:15 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/ProductCategory.java
+++ src/main/java/it/ecommerce/bookshop/model/ProductCategory.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "product_category")
+@Table(name = "product_categories")
 public class ProductCategory {
 	
 	@Id
