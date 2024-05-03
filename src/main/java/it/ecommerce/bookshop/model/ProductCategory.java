e77d437dba960df017d79cc8362f4c7be7f2f8d9
M'bazie Kone <mbazie89@gmail.com>
2024-05-03 23:08:32 +0200

    Updates

---
+++ src/main/java/it/ecommerce/bookshop/model/ProductCategory.java
@@ -0,0 +1,12 @@
+package it.ecommerce.bookshop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product_category")
+public class ProductCategory {
+	
+	
+	
+}
