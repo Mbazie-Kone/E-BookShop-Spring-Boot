eada849339e52a0ad2946e343865cd76226f662e
M'bazie Kone <mbazie89@gmail.com>
2024-05-03 23:10:59 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/ProductCategory.java
+++ src/main/java/it/ecommerce/bookshop/model/ProductCategory.java
@@ -1,12 +1,19 @@
 package it.ecommerce.bookshop.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "product_category")
 public class ProductCategory {
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "product_category_id")
+	private Long id;
 	
 }
