ea8e1d4b89a0e53d8627ab79f5ded1aeaf18cef6
M'bazie Kone <mbazie89@gmail.com>
2024-05-04 02:43:13 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/config/DataRestConfig.java
+++ src/main/java/it/ecommerce/bookshop/config/DataRestConfig.java
@@ -1,12 +1,15 @@
 package it.ecommerce.bookshop.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import it.ecommerce.bookshop.model.Product;
+import it.ecommerce.bookshop.model.ProductCategory;
 
+@Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
 
 	@Override
@@ -16,6 +19,10 @@ public class DataRestConfig implements RepositoryRestConfigurer {
 		
 		// disable HTTP methods for Product: PUT, POST and DELETE
 		config.getExposureConfiguration().forDomainType(Product.class).withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+		.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+		
+		// disable HTTP methods for ProductCategory: PUT, POST and DELETE
+		config.getExposureConfiguration().forDomainType(ProductCategory.class).withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
 		.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 	}
 }
