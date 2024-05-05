ab6db7cc6dd8b351ebb2aec65a9098845251ebf7
M'bazie Kone <mbazie89@gmail.com>
2024-05-05 23:51:58 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/services/product.service.ts
+++ front-end/e-bookshop-frontend/src/app/services/product.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Product } from '../common/product';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
