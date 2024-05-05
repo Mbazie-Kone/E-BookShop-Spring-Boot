1d352f51cb4bbee5ab37ce64e0705fcb3b114bac
M'bazie Kone <mbazie89@gmail.com>
2024-05-05 23:43:35 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/services/product.service.ts
+++ front-end/e-bookshop-frontend/src/app/services/product.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +11,8 @@ export class ProductService {
   private baseUrl = 'http:localhost:8080/api/products';
 
   constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GET
+  }
 }
