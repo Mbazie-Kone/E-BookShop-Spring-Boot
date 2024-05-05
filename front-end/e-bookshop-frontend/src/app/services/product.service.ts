a5b8e12ee031c0005ca72332b05618806bea4795
M'bazie Kone <mbazie89@gmail.com>
2024-05-05 23:37:14 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/services/product.service.ts
+++ front-end/e-bookshop-frontend/src/app/services/product.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ProductService {
 
-  private baseUrl = ""
+  private baseUrl = 'http:localhost:8080/api/products';
 
   constructor() { }
 }
