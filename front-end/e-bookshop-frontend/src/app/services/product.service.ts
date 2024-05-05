aa144fa566baf2117ffd9142f30e5628baa96e5b
M'bazie Kone <mbazie89@gmail.com>
2024-05-05 23:40:22 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/services/product.service.ts
+++ front-end/e-bookshop-frontend/src/app/services/product.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,5 +8,5 @@ export class ProductService {
 
   private baseUrl = 'http:localhost:8080/api/products';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 }
