1eabbe57ff9b76d68ea2092ddc909708fcaef23a
M'bazie Kone <mbazie89@gmail.com>
2024-05-06 22:03:54 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/repository/ProductRepository.java
+++ src/main/java/it/ecommerce/bookshop/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package it.ecommerce.bookshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import it.ecommerce.bookshop.model.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
