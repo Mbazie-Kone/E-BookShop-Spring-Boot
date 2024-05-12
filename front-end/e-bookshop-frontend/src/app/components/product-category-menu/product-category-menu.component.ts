4f36a7a5be18e29dc1cd2e40dbbe66a96d3774a2
M'bazie Kone <mbazie89@gmail.com>
2024-05-12 09:44:22 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/product-category-menu/product-category-menu.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductCategory } from '../../common/product-category';
 
 @Component({
   selector: 'app-product-category-menu',
   templateUrl: './product-category-menu.component.html',
   styleUrl: './product-category-menu.component.css'
 })
-export class ProductCategoryMenuComponent {
+export class ProductCategoryMenuComponent implements OnInit {
 
+  productCategories: ProductCategory[];
+
+  constructor() {}
+
+
+  ngOnInit(): void {
+
+  }
 }
