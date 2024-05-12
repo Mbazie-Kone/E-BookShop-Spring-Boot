ae3d86581a9ea9b8f616fc051590b64d5d5fb1ce
M'bazie Kone <mbazie89@gmail.com>
2024-05-12 08:12:27 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/ProductCategory.java
+++ src/main/java/it/ecommerce/bookshop/model/ProductCategory.java
@@ -1,6 +1,7 @@
 package it.ecommerce.bookshop.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -11,15 +12,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
 
-// We will use the Lombok setup for this entity
+
 @Entity
 @Table(name = "product_categories")
-// @Data -- known bug
-@Getter
-@Setter
 public class ProductCategory implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -34,4 +30,37 @@ public class ProductCategory implements Serializable {
 	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
 	private Set<Product> products;
+
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(categoryName, id, products);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductCategory other = (ProductCategory) obj;
+		return Objects.equals(categoryName, other.categoryName) && Objects.equals(id, other.id)
+				&& Objects.equals(products, other.products);
+	}
+
+	@Override
+	public String toString() {
+		return "ProductCategory [id=" + id + ", categoryName=" + categoryName + ", products=" + products + "]";
+	}
+	
 }
