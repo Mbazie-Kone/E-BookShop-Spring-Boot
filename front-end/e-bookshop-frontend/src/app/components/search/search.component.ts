9416a7a1bec1e3e78db3a16fbcae42aeace13f52
M'bazie Kone <mbazie89@gmail.com>
2024-05-14 01:08:38 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/search/search.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,11 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() {
+  constructor(private route: Router) {
 
   }
 
   ngOnInit(): void {
+    
+  }
+
+  doSearch() {
     
   }
 
