916472f473cb9b2e204a8c72890343e18e5014d8
M'bazie Kone <mbazie89@gmail.com>
2024-05-14 01:19:52 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/product-list/product-list.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  searchMode: boolean = false;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute,
@@ -27,23 +28,29 @@ export class ProductListComponent implements OnInit {
   
   listProducts() {
 
-    // check if "id" parameter is available
-    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
-    if(hasCategoryId) {
-      // get the "id" param string convert strint to number using the "+" symbol
-      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
-    }
-    else {
-      // not category id available ... default to category 1
-      this.currentCategoryId = 1;
-    }
+    this.handleListProduct();
+  }
 
-    // now get the products for given the category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    );
+  handleListProduct() {
+     // check if "id" parameter is available
+     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+     if(hasCategoryId) {
+       // get the "id" param string convert strint to number using the "+" symbol
+       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+     }
+     else {
+       // not category id available ... default to category 1
+       this.currentCategoryId = 1;
+     }
+ 
+     // now get the products for given the category id
+     this.productService.getProductList(this.currentCategoryId).subscribe(
+       data => {
+         this.products = data;
+       }
+     );
   }
 }
