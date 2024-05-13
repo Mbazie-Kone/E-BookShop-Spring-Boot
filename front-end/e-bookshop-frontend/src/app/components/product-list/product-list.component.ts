8193b7805cd99e1ea3bb3731a8c79f8e0ac4d48c
M'bazie Kone <mbazie89@gmail.com>
2024-05-14 01:23:42 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/product-list/product-list.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/product-list/product-list.component.ts
@@ -6,51 +6,51 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent implements OnInit {
-
   products: Product[];
   currentCategoryId: number;
   searchMode: boolean = false;
 
-  constructor(private productService: ProductService,
-              private route: ActivatedRoute,
-  ) {
-
-  }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
   }
-  
-  listProducts() {
 
+  listProducts() {
     this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
-    this.handleListProduct();
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProduct();
+    }
   }
 
   handleListProduct() {
-     // check if "id" parameter is available
-     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
-     if(hasCategoryId) {
-       // get the "id" param string convert strint to number using the "+" symbol
-       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
-     }
-     else {
-       // not category id available ... default to category 1
-       this.currentCategoryId = 1;
-     }
- 
-     // now get the products for given the category id
-     this.productService.getProductList(this.currentCategoryId).subscribe(
-       data => {
-         this.products = data;
-       }
-     );
+    if (hasCategoryId) {
+      // get the "id" param string convert strint to number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      // not category id available ... default to category 1
+      this.currentCategoryId = 1;
+    }
+
+    // now get the products for given the category id
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe((data) => {
+        this.products = data;
+      });
   }
 }
