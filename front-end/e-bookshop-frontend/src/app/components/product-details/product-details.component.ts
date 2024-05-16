e8ec71263bdc52ff580faaf36259f1c8cf135ce4
M'bazie Kone <mbazie89@gmail.com>
2024-05-16 23:45:34 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/product-details/product-details.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+ ;
+  }
 
 }
