6bda5623df59a73cd5324df2a7c44699c82a198e
M'bazie Kone <mbazie89@gmail.com>
2024-05-18 06:47:40 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/product-list/product-list.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/product-list/product-list.component.ts
@@ -77,7 +77,9 @@ export class ProductListComponent implements OnInit {
     console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
 
     // now get the products for given the category id
-    this.productService.getProductListPaginate(this.thePageNumber = - 1,this.thePageSize, this.currentCategoryId)
+    this.productService.getProductListPaginate(this.thePageNumber = - 1,this.thePageSize, this.currentCategoryId).subscribe(data => {
+      this.products = data._embedded.products; this.thePageNumber = data.page.number + 1; this.thePageSize = data.page.size; this.theTotalElements = data.page.totalElements;
+    });
      
   }
 }
