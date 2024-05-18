2470f83a36f86f8ac28003b8133820c32d192d8e
M'bazie Kone <mbazie89@gmail.com>
2024-05-18 06:08:03 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/services/product.service.ts
+++ front-end/e-bookshop-frontend/src/app/services/product.service.ts
@@ -52,7 +52,13 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
-  };
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
+  }
 }
 
 interface GetResponseProductCategory {
