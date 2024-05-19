7722c2283cc0e838cb83e4fed12550062a2bed46
M'bazie Kone <mbazie89@gmail.com>
2024-05-20 01:22:29 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/product-list/product-list.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/product-list/product-list.component.ts
@@ -18,6 +18,8 @@ export class ProductListComponent implements OnInit {
   thePageNumber: number = 1;
   thePageSize: number = 5;
   theTotalElements: number = 0;
+
+  previousKeyword: string = "";
  
   constructor(
     private productService: ProductService,
@@ -42,6 +44,12 @@ export class ProductListComponent implements OnInit {
 
   handleSearchProducts() {
     const theKeyWord: string = this.route.snapshot.paramMap.get('keyword')!;
+    // if we have a different keyword than previous
+    // then set thePageNumber to 1
+    if(this.previousKeyword != theKeyWord) {
+      this.thePageNumber = 1;
+    }
+
     // now search for the products using keyword
     this.productService.searchProducts(theKeyWord).subscribe(
       data => {
