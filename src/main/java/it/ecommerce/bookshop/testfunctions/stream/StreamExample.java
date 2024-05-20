78a9b08897d80986b2fa6b7f204b46af5c2f4233
M'bazie Kone <mbazie89@gmail.com>
2024-05-21 00:02:41 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/testfunctions/stream/StreamExample.java
+++ src/main/java/it/ecommerce/bookshop/testfunctions/stream/StreamExample.java
@@ -2,9 +2,13 @@ package it.ecommerce.bookshop.testfunctions.stream;
 
 public class StreamExample {
 
+	public void test(String a) {
+		System.out.println(a);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		StreamExample streamExample = new StreamExample();
 	}
 
 }
