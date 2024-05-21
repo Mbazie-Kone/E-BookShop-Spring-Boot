04f5128a41d8f169e7fa4879e55f0aef03935b6c
M'bazie Kone <mbazie89@gmail.com>
2024-05-21 20:30:33 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/testfunctions/stream/StreamExample.java
+++ src/main/java/it/ecommerce/bookshop/testfunctions/stream/StreamExample.java
@@ -1,22 +1,7 @@
 package it.ecommerce.bookshop.testfunctions.stream;
 
-import java.util.Arrays;
-import java.util.List;
 
 public class StreamExample {
 
-	public void test(String a) {
-		System.out.println(a);
-	}
-	
-	public static void main(String[] args) {
-		
-		StreamExample streamExample = new StreamExample();
-		
-		List<String> textList = Arrays.asList("Wow", "Wow", "Wow", "Hi", "Hello", "Bye");
-		
-		textList.stream().filter(b -> b == "Wow").forEach(streamExample::test);
-		
-	}
 
 }
