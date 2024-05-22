5365f797233fc01dc3d5b050e778a72783f6e992
M'bazie Kone <mbazie89@gmail.com>
2024-05-22 03:07:21 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/testfunctions/stream/StreamExample.java
+++ src/main/java/it/ecommerce/bookshop/testfunctions/stream/StreamExample.java
@@ -1,18 +1,8 @@
 package it.ecommerce.bookshop.testfunctions.stream;
 
-
 public class StreamExample {
-	
-	public static void main(String[] args) {
-		
-		String value = "red";
-		switch (value) {
-		case "red": {
-			System.out.println("FAIL");
-		}
-		case "green":
-			System.out.println("OK");
-		}
-	}
 
+	public static void main(String[] args) {
+
+	}
 }
