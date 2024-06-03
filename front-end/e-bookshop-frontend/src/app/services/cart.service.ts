2b1870ec88a83cfcb4a1903d167991df49ffec19
M'bazie Kone <mbazie89@gmail.com>
2024-06-04 00:21:30 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/services/cart.service.ts
+++ front-end/e-bookshop-frontend/src/app/services/cart.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
 
   constructor() { }
 }
