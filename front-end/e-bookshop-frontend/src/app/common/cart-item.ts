c41d9713b5038e6fd6b0a1c4391dc847a50465a0
M'bazie Kone <mbazie89@gmail.com>
2024-06-04 00:14:14 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/common/cart-item.ts
+++ front-end/e-bookshop-frontend/src/app/common/cart-item.ts
@@ -1,6 +1,17 @@
+import { Product } from "./product";
+
 export class CartItem {
-    id: string;
+    id: number;
     name: string;
     imageUrl: string;
     unitPrince: number;
+
+    quantity: number;
+
+    constructor(product: Product) {
+        this.id = product.id;
+        this.name = product.name;
+        this.imageUrl = product.imageUrl;
+        this.unitPrince = product.unitPrice;
+    }
 }
