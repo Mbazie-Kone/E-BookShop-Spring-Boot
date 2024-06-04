390e5f76518a330e29d49c65e15653aacb5a8be2
M'bazie Kone <mbazie89@gmail.com>
2024-06-04 02:02:37 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/product-details/product-details.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +13,7 @@ export class ProductDetailsComponent implements OnInit {
 
   product!: Product;
 
-  constructor(private productService: ProductService, private route: ActivatedRoute){
+  constructor(private productService: ProductService, private cartService: CartService, private route: ActivatedRoute){
 
   }
 
@@ -33,4 +34,7 @@ export class ProductDetailsComponent implements OnInit {
    );
   }
 
+  addToCart() {
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+  }
 }
