02a13488830f3694edf7ba985defbf7fa7a2bc2d
M'bazie Kone <mbazie89@gmail.com>
2024-06-12 02:58:48 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/app.module.ts
+++ front-end/e-bookshop-frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 const routes: Routes = [
   {path: 'products/:id', component: ProductDetailsComponent},
@@ -33,6 +34,7 @@ const routes: Routes = [
     SearchComponent,
     ProductDetailsComponent,
     CartStatusComponent,
+    CartDetailsComponent,
   ],
   imports: [
     RouterModule.forRoot(routes),
