c5f6eca78cdba7eebedd16c202714880e44d3807
M'bazie Kone <mbazie89@gmail.com>
2024-06-12 03:27:08 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/cart-details/cart-details.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/cart-details/cart-details.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from '../../common/cart-item';
 
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
   styleUrl: './cart-details.component.css'
 })
-export class CartDetailsComponent {
+export class CartDetailsComponent implements OnInit {
+ 
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  
+  }
 
 }
