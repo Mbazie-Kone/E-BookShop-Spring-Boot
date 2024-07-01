4d80576625f03ca2e7fe795cb83de6ef15531986
M'bazie Kone <mbazie89@gmail.com>
2024-07-01 02:22:06 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/cart-details/cart-details.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/cart-details/cart-details.component.ts
@@ -36,5 +36,8 @@ export class CartDetailsComponent implements OnInit {
     // compute cart total price and quantity
     this.cartService.computeCartTotals();
   }
+    incrementQuantity(theCartItem: CartItem) {
+      this.cartService.addToCart(theCartItem);
+    }
 
 }
