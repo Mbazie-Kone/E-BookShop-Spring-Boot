bc8a50af816c749bbba33a042ddf03361c3cae60
M'bazie Kone <mbazie89@gmail.com>
2024-07-05 22:42:45 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/Country.java
+++ src/main/java/it/ecommerce/bookshop/model/Country.java
@@ -3,8 +3,10 @@ package it.ecommerce.bookshop.model;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "countries")
 public class Country implements Serializable {
 
 	private static final long serialVersionUID = 1L;
