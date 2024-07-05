1d6cb7b9dadcdf046e2619b237c858dd8184db0c
M'bazie Kone <mbazie89@gmail.com>
2024-07-05 23:23:10 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/State.java
+++ src/main/java/it/ecommerce/bookshop/model/State.java
@@ -2,6 +2,7 @@ package it.ecommerce.bookshop.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class State implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "state_id")
 	private Long id;
 	
 	private String name;
