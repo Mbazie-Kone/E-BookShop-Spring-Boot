ae20c2666085a11ed73ea3b7338eae9ad30c2796
M'bazie Kone <mbazie89@gmail.com>
2024-07-06 01:36:58 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/repository/CountryRepository.java
+++ src/main/java/it/ecommerce/bookshop/repository/CountryRepository.java
@@ -1,5 +1,9 @@
 package it.ecommerce.bookshop.repository;
 
-public interface CountryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import it.ecommerce.bookshop.model.Country;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
 
 }
