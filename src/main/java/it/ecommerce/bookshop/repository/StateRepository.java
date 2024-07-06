ab3a0d640992e6f6f059d6db4c59e5c0a7e37399
M'bazie Kone <mbazie89@gmail.com>
2024-07-06 02:29:00 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/repository/StateRepository.java
+++ src/main/java/it/ecommerce/bookshop/repository/StateRepository.java
@@ -1,11 +1,18 @@
 package it.ecommerce.bookshop.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import it.ecommerce.bookshop.model.State;
 
 @CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
 public interface StateRepository extends JpaRepository<State, Long>{
+	
+	public List<State> findByCountryCode(@Param("code") String code);
 
 }
