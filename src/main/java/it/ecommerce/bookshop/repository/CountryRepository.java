2d852da04497682bcaa79643c468a87f89c02904
M'bazie Kone <mbazie89@gmail.com>
2024-07-06 02:04:51 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/repository/CountryRepository.java
+++ src/main/java/it/ecommerce/bookshop/repository/CountryRepository.java
@@ -1,11 +1,13 @@
 package it.ecommerce.bookshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import it.ecommerce.bookshop.model.Country;
 
 @CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
 public interface CountryRepository extends JpaRepository<Country, Long> {
 
 }
