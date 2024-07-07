74044cb12a15a5411352d8595d313457bf268f97
M'bazie Kone <mbazie89@gmail.com>
2024-07-07 21:26:07 +0200

    updates

--- front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
@@ -100,6 +100,7 @@ export class CheckoutComponent implements OnInit{
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log("the email address is "+ this.checkoutFormGroup.get('customer')?.value.email);
+    console.log("the shipping address is "+ this.checkoutFormGroup.get('customer')?.value.email);
   }
 
   handleMonthsAndYears() {
