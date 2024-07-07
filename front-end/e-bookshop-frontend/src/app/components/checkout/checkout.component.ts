f53b4aec0759fc0f356b2b64475f35dd544a1feb
M'bazie Kone <mbazie89@gmail.com>
2024-07-08 01:36:28 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
@@ -106,6 +106,9 @@ export class CheckoutComponent implements OnInit{
 
   onSubmit() {
     console.log("Handling the submit button");
+    if(this.checkoutFormGroup.invalid) {
+
+    }
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log("the email address is "+ this.checkoutFormGroup.get('customer')?.value.email);
     console.log("the shipping address country is "+ this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
