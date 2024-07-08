d95beae48944f464478818dc9d48b97b714e674b
M'bazie Kone <mbazie89@gmail.com>
2024-07-08 03:01:02 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/validators/ebook-shop-validators.ts
+++ front-end/e-bookshop-frontend/src/app/validators/ebook-shop-validators.ts
@@ -6,7 +6,8 @@ export class EbookShopValidators {
     static notOnlyWhitespace(control: FormControl) : ValidationErrors {
         // check if string only contains whitespace
         if((control.value != null) && (control.value.trim().length === 0)) {
-
+            // ivalid, return error object
+            return { 'notOnlyWhitespace': true};
         }
         return null;
     }
