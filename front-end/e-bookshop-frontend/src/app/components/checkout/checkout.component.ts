3511b96a3bde318a932c813148f90970f3d61e3d
M'bazie Kone <mbazie89@gmail.com>
2024-07-08 03:39:22 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
@@ -88,6 +88,7 @@ export class CheckoutComponent implements OnInit{
   get firstName() {return this.checkoutFormGroup.get('customer.firstName');}
   get lastName() {return this.checkoutFormGroup.get('customer.lastName');}
   get email() {return this.checkoutFormGroup.get('customer.email');}
+  get shippingAddressStreet() {return this.checkoutFormGroup.get('customer.email');}
 
   copyShippingAddressToBillingAddress(event:any):void {
     if(event.target.checked) {
