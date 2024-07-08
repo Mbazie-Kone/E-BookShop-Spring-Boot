73099e02966f6c52a657db6cb0c75edbe74d9b51
M'bazie Kone <mbazie89@gmail.com>
2024-07-09 00:57:21 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
@@ -95,11 +95,11 @@ export class CheckoutComponent implements OnInit{
   get shippingAddressCountry() {return this.checkoutFormGroup.get('shippingAddress.country');}
   get shippingAddressZipCode() {return this.checkoutFormGroup.get('shippingAddress.zipCode');}
 
-  get billingAddressStreet() {return this.checkoutFormGroup.get('shippingAddress.street');}
-  get billingAddressCity() {return this.checkoutFormGroup.get('shippingAddress.city');}
-  get billingAddressState() {return this.checkoutFormGroup.get('shippingAddress.state');}
-  get billingAddressCountry() {return this.checkoutFormGroup.get('shippingAddress.country');}
-  get billingAddressZipCode() {return this.checkoutFormGroup.get('shippingAddress.zipCode');}
+  get billingAddressStreet() {return this.checkoutFormGroup.get('billingAddress.street');}
+  get billingAddressCity() {return this.checkoutFormGroup.get('billingAddress.city');}
+  get billingAddressState() {return this.checkoutFormGroup.get('billingAddress.state');}
+  get billingAddressCountry() {return this.checkoutFormGroup.get('billingAddress.country');}
+  get billingAddressZipCode() {return this.checkoutFormGroup.get('billingAddress.zipCode');}
 
 
   copyShippingAddressToBillingAddress(event:any):void {
