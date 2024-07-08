56d56f56dbdc592a8c3fb331b41833c13ad54717
M'bazie Kone <mbazie89@gmail.com>
2024-07-08 03:10:11 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/validators/ebook-shop-validators.ts
+++ front-end/e-bookshop-frontend/src/app/validators/ebook-shop-validators.ts
@@ -3,7 +3,7 @@ import { FormControl, ValidationErrors } from "@angular/forms";
 export class EbookShopValidators {
 
     // whitespace validator
-    static notOnlyWhitespace(control: FormControl) : ValidationErrors {
+    static notOnlyWhitespace(control: FormControl) : ValidationErrors | null {
         // check if string only contains whitespace
         if((control.value != null) && (control.value.trim().length === 0)) {
             // ivalid, return error object
