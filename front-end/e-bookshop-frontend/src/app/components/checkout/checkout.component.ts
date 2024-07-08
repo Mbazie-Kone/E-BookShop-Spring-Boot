65badead4080f05d40b67ecc55611213280d2388
M'bazie Kone <mbazie89@gmail.com>
2024-07-09 01:51:22 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
@@ -51,7 +51,7 @@ export class CheckoutComponent implements OnInit{
       creditCard: this.formBuilder.group({
         cardType: new FormControl('', [Validators.required]),
         nameOnCard: new FormControl('', [Validators.required, Validators.minLength(2), EbookShopValidators.notOnlyWhitespace]),
-        cardNumber: new FormControl('', [Validators.pattern('[0-9]')]),
+        cardNumber: new FormControl('', [Validators.pattern('[0-9]{16}')]),
         securityCode: new FormControl('', [Validators.required]),
         expirationMonth: new FormControl('', [Validators.required]),
         expirationYear: new FormControl('', [Validators.required])
