c068e01c32e2beade4031bd5a0d3cc51e1a3df18
M'bazie Kone <mbazie89@gmail.com>
2024-07-08 03:32:48 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
@@ -35,11 +35,11 @@ export class CheckoutComponent implements OnInit{
         email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
-        street: [''],
-        city: [''],
-        state: [''],
-        country: [''],
-        zipCode: ['']
+        street: new FormControl('', [Validators.required, Validators.minLength(2), EbookShopValidators.notOnlyWhitespace]),
+        city: new FormControl('', [Validators.required, Validators.minLength(2), EbookShopValidators.notOnlyWhitespace]),
+        state: new FormControl('', [Validators.required, Validators.minLength(2), EbookShopValidators.notOnlyWhitespace]),
+        country: new FormControl('', [Validators.required, Validators.minLength(2), EbookShopValidators.notOnlyWhitespace]),
+        zipCode: new FormControl('', [Validators.required, Validators.minLength(2), EbookShopValidators.notOnlyWhitespace])
       }),
       billingAddress: this.formBuilder.group({
         street: [''],
