8b5e6a18087909f99a16ab7529104de2482c2754
M'bazie Kone <mbazie89@gmail.com>
2024-07-10 01:58:49 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/Customer.java
+++ src/main/java/it/ecommerce/bookshop/model/Customer.java
@@ -2,7 +2,11 @@ package it.ecommerce.bookshop.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,9 +15,16 @@ public class Customer implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "customer_id")
 	private Long id;
 	
+	@Column(name = "first_name")
 	private String firstName;
 	
+	@Column(name = "last_name")
 	private String lastName;
+	
+	private String email;
 }
