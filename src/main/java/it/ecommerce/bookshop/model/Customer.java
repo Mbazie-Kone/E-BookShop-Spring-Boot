f5b3982da4e50004a3c6d280da3778cc08ae7280
M'bazie Kone <mbazie89@gmail.com>
2024-07-10 01:54:09 +0200

    Updates

---
+++ src/main/java/it/ecommerce/bookshop/model/Customer.java
@@ -0,0 +1,14 @@
+package it.ecommerce.bookshop.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
