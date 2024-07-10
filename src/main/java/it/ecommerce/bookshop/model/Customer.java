74dfcf9119a6c6a757b8f6a73063bb385924f59e
M'bazie Kone <mbazie89@gmail.com>
2024-07-10 02:03:48 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/Customer.java
+++ src/main/java/it/ecommerce/bookshop/model/Customer.java
@@ -38,6 +38,30 @@ public class Customer implements Serializable {
 		this.lastName = lastName;
 		this.email = email;
 	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	
 }
