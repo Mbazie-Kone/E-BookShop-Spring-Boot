743b3442cc7b6b90efb15469236d43600c1165f9
M'bazie Kone <mbazie89@gmail.com>
2024-07-10 02:07:37 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/Address.java
+++ src/main/java/it/ecommerce/bookshop/model/Address.java
@@ -1,10 +1,14 @@
 package it.ecommerce.bookshop.model;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "addresses")
-public class Address {
+public class Address implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 }
