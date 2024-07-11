0a379503777e7c71bef5fb15e26ffef50060a885
M'bazie Kone <mbazie89@gmail.com>
2024-07-11 21:35:06 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/Order.java
+++ src/main/java/it/ecommerce/bookshop/model/Order.java
@@ -9,4 +9,8 @@ import jakarta.persistence.Table;
 @Table(name = "orders")
 public class Order implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+
 }
