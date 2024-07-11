b3d3b79bba3f5908de8e9eefc90be201bed7bafa
M'bazie Kone <mbazie89@gmail.com>
2024-07-11 21:36:33 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/Order.java
+++ src/main/java/it/ecommerce/bookshop/model/Order.java
@@ -2,6 +2,7 @@ package it.ecommerce.bookshop.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class Order implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "order_id")
 	private Long id;
 
 }
