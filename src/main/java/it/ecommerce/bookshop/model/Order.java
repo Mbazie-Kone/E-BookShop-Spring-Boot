11bf73eb0e38417127cb094deb54d3a935fa924f
M'bazie Kone <mbazie89@gmail.com>
2024-07-11 21:39:15 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/Order.java
+++ src/main/java/it/ecommerce/bookshop/model/Order.java
@@ -19,5 +19,8 @@ public class Order implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "order_id")
 	private Long id;
+	
+	@Column(name = "order_tracking_number")
+	private String orderTrackingNumber;
 
 }
