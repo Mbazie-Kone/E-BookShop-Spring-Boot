0d1aaaa72e28595f7ec2a7d0d8f9d994e0e989b9
M'bazie Kone <mbazie89@gmail.com>
2024-07-14 20:29:01 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/dto/Purchase.java
+++ src/main/java/it/ecommerce/bookshop/dto/Purchase.java
@@ -1,5 +1,6 @@
 package it.ecommerce.bookshop.dto;
 
+import it.ecommerce.bookshop.model.Address;
 import it.ecommerce.bookshop.model.Customer;
 import lombok.Data;
 
@@ -7,5 +8,7 @@ import lombok.Data;
 public class Purchase {
 	
 	private Customer customer;
+	
+	private Address shippingAddress;
 
 }
