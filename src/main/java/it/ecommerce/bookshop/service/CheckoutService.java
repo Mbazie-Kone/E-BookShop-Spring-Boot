71170873482d5bfa0b7f21520f58b66e41969f47
M'bazie Kone <mbazie89@gmail.com>
2024-07-15 23:34:06 +0200

    Udpates

--- src/main/java/it/ecommerce/bookshop/service/CheckoutService.java
+++ src/main/java/it/ecommerce/bookshop/service/CheckoutService.java
@@ -1,9 +1,10 @@
 package it.ecommerce.bookshop.service;
 
+import it.ecommerce.bookshop.dto.Purchase;
 import it.ecommerce.bookshop.dto.PurchaseResponse;
 
 public interface CheckoutService {
 	
-	PurchaseResponse placeOrder()
+	PurchaseResponse placeOrder(Purchase purchase)
 
 }
