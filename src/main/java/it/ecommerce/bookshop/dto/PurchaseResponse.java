a474bbb2eddb073768567d351c174670c0202546
M'bazie Kone <mbazie89@gmail.com>
2024-07-16 01:25:34 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/dto/PurchaseResponse.java
+++ src/main/java/it/ecommerce/bookshop/dto/PurchaseResponse.java
@@ -6,7 +6,13 @@ public class PurchaseResponse {
 	
 	@NonNull
 	private String orderTrackingNumber;
-	
-	
+
+	public String getOrderTrackingNumber() {
+		return orderTrackingNumber;
+	}
+
+	public void setOrderTrackingNumber(String orderTrackingNumber) {
+		this.orderTrackingNumber = orderTrackingNumber;
+	}
 
 }
