15eb29ba85b25635268968e99d7ae7414ab1c878
M'bazie Kone <mbazie89@gmail.com>
2024-07-16 01:35:04 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/controller/CheckoutController.java
+++ src/main/java/it/ecommerce/bookshop/controller/CheckoutController.java
@@ -1,8 +1,10 @@
 package it.ecommerce.bookshop.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin("")
+@CrossOrigin("http://localhost:4200")
+@RestController
 public class CheckoutController {
 
 }
