4b61475beab047ce790c7b74ccd724e8b01cef2b
M'bazie Kone <mbazie89@gmail.com>
2024-07-15 23:44:00 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/service/impl/CheckoutServiceImpl.java
+++ src/main/java/it/ecommerce/bookshop/service/impl/CheckoutServiceImpl.java
@@ -4,10 +4,13 @@ import org.springframework.stereotype.Service;
 
 import it.ecommerce.bookshop.dto.Purchase;
 import it.ecommerce.bookshop.dto.PurchaseResponse;
+import it.ecommerce.bookshop.repository.CustomerRepository;
 import it.ecommerce.bookshop.service.CheckoutService;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
+	
+	private CustomerRepository customerRepository;
 
 	@Override
 	public PurchaseResponse placeOrder(Purchase purchase) {
