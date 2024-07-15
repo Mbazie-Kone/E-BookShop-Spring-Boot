9acc150d0ec6eaae0ea4eaa515629f3f2e5490e6
M'bazie Kone <mbazie89@gmail.com>
2024-07-16 01:47:19 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/controller/CheckoutController.java
+++ src/main/java/it/ecommerce/bookshop/controller/CheckoutController.java
@@ -24,7 +24,6 @@ public class CheckoutController {
 	@PostMapping("/purchase")
 	public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
 		PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+		return purchaseResponse;
 	}
-	
-
 }
