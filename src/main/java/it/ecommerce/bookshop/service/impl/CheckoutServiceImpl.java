400ee4a352c72be6f02ee8eeb07afac08c9429db
M'bazie Kone <mbazie89@gmail.com>
2024-07-15 23:50:44 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/service/impl/CheckoutServiceImpl.java
+++ src/main/java/it/ecommerce/bookshop/service/impl/CheckoutServiceImpl.java
@@ -1,12 +1,12 @@
 package it.ecommerce.bookshop.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import it.ecommerce.bookshop.dto.Purchase;
 import it.ecommerce.bookshop.dto.PurchaseResponse;
 import it.ecommerce.bookshop.repository.CustomerRepository;
 import it.ecommerce.bookshop.service.CheckoutService;
+import jakarta.transaction.Transactional;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
@@ -18,6 +18,7 @@ public class CheckoutServiceImpl implements CheckoutService {
 	}
 
 	@Override
+	@Transactional
 	public PurchaseResponse placeOrder(Purchase purchase) {
 		// TODO Auto-generated method stub
 		return null;
