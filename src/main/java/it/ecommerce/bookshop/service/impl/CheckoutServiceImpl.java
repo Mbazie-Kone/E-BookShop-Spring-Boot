2bb4d9c76e5468d54685a6fa6ab3437ac872ffd8
M'bazie Kone <mbazie89@gmail.com>
2024-07-15 23:38:51 +0200

    Updates

---
+++ src/main/java/it/ecommerce/bookshop/service/impl/CheckoutServiceImpl.java
@@ -0,0 +1,15 @@
+package it.ecommerce.bookshop.service.impl;
+
+import it.ecommerce.bookshop.dto.Purchase;
+import it.ecommerce.bookshop.dto.PurchaseResponse;
+import it.ecommerce.bookshop.service.CheckoutService;
+
+public class CheckoutServiceImpl implements CheckoutService {
+
+	@Override
+	public PurchaseResponse placeOrder(Purchase purchase) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
