dd68be78c62bfb89f6dae813b5a639d04b8ab814
M'bazie Kone <mbazie89@gmail.com>
2024-07-16 00:30:03 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/dto/PurchaseResponse.java
+++ src/main/java/it/ecommerce/bookshop/dto/PurchaseResponse.java
@@ -1,5 +1,7 @@
 package it.ecommerce.bookshop.dto;
 
+import java.util.Objects;
+
 public class PurchaseResponse {
 	
 	private String orderTrackingNumber;
@@ -11,7 +13,27 @@ public class PurchaseResponse {
 	public void setOrderTrackingNumber(String orderTrackingNumber) {
 		this.orderTrackingNumber = orderTrackingNumber;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(orderTrackingNumber);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PurchaseResponse other = (PurchaseResponse) obj;
+		return Objects.equals(orderTrackingNumber, other.orderTrackingNumber);
+	}
+
+	@Override
+	public String toString() {
+		return "PurchaseResponse [orderTrackingNumber=" + orderTrackingNumber + "]";
+	}
 
 }
