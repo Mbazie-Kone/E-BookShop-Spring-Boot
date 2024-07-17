9c7f23c5a925c18c0bc526e3aea2e9bf3afeda3c
M'bazie Kone <mbazie89@gmail.com>
2024-07-18 00:53:04 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/services/checkout.service.ts
+++ front-end/e-bookshop-frontend/src/app/services/checkout.service.ts
@@ -5,6 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class CheckoutService {
 
-  private purchaseUrl = 'http://localhost:8080'
+  private purchaseUrl = 'http://localhost:8080/api/checkout/purchase'
   constructor() { }
 }
