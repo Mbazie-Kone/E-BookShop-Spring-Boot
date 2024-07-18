f1b3d6bfc9a302df1f1c70fc0469bd5539cd1258
M'bazie Kone <mbazie89@gmail.com>
2024-07-18 05:15:46 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
@@ -147,10 +147,7 @@ export class CheckoutComponent implements OnInit{
       this.checkoutFormGroup.markAllAsTouched();
       return;
     }
-    console.log(this.checkoutFormGroup.get('customer')?.value);
-    console.log("the email address is "+ this.checkoutFormGroup.get('customer')?.value.email);
-    console.log("the shipping address country is "+ this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
-    console.log("the shipping address state is "+ this.checkoutFormGroup.get('shippingAddress')?.value.state.name);
+   
   }
 
   handleMonthsAndYears() {
