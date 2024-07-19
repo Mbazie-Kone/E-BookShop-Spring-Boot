e81257ca29a5460d262e00af884ee679ef366d0b
M'bazie Kone <mbazie89@gmail.com>
2024-07-19 22:49:07 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/checkout/checkout.component.ts
@@ -197,7 +197,7 @@ export class CheckoutComponent implements OnInit{
    this.checkoutService.placeOrder(purchase).subscribe(
     {
       next: responese => {
-        alert(`Your order has been received.\nOrder tracking number: ${responese.orderTrackingNumber}`)
+        alert(`Your order has been received.\nOrder tracking number: ${responese.orderTrackingNumber}`);
       },
       error:
     }
