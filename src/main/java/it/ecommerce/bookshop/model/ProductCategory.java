e23bf54e1b735feadd46a6d5cb0b6fcf8c7fe165
M'bazie Kone <mbazie89@gmail.com>
2024-07-21 23:56:03 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/ProductCategory.java
+++ src/main/java/it/ecommerce/bookshop/model/ProductCategory.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -22,6 +24,7 @@ public class ProductCategory implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonProperty("id")
 	private Long id;
 	
 	@Column(name = "category_name")
