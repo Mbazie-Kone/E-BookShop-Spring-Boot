c5b0738cab0e3a0368a3705526bf6fb9eddf5290
M'bazie Kone <mbazie89@gmail.com>
2024-07-22 02:59:03 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/login/login.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 
 import myAppConfig from '../../config/my-app-config';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,7 @@ export class LoginComponent implements OnInit {
 
   oktaSignin: any;
 
-  constructor() {}
+  constructor(@inject(OKTA_AUTH) private oktaAuth: OktaAuth) {}
 
   ngOnInit(): void {
     
