bab6611c616fcb940e063b1f95f17a1040a203ee
M'bazie Kone <mbazie89@gmail.com>
2024-07-22 02:28:57 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/config/my-app-config.ts
+++ front-end/e-bookshop-frontend/src/app/config/my-app-config.ts
@@ -1,6 +1,6 @@
 export default {
     oidc: {
-        clientId: '',
+        clientId: '0oaih5kddeasiJFbq5d7',
         issuer: '',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
