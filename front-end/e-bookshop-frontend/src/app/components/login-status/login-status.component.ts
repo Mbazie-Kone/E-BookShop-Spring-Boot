02d54dd8efc727b53b48c1aa4c657ee892b7cb5f
M'bazie Kone <mbazie89@gmail.com>
2024-07-22 03:41:17 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/login-status/login-status.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/login-status/login-status.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-status',
   templateUrl: './login-status.component.html',
   styleUrl: './login-status.component.css'
 })
-export class LoginStatusComponent {
+export class LoginStatusComponent implements OnInit {
+
+
+  
+  ngOnInit(): void {
+    
+  }
 
 }
