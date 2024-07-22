cbf2beae81e7bac16b583045589529d936a13316
M'bazie Kone <mbazie89@gmail.com>
2024-07-22 04:02:59 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/components/login-status/login-status.component.ts
+++ front-end/e-bookshop-frontend/src/app/components/login-status/login-status.component.ts
@@ -37,7 +37,8 @@ export class LoginStatusComponent implements OnInit {
   }
 
   logout() {
-    
+    // terminates the session with Okta and removes current tokens
+    this.oktaAuth.signOut();
   }
 
 }
