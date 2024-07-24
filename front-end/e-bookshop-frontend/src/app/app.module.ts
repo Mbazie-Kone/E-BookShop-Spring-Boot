c94b1b6f13063904e49db806713e378a36109914
M'bazie Kone <mbazie89@gmail.com>
2024-07-25 00:34:12 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/app.module.ts
+++ front-end/e-bookshop-frontend/src/app/app.module.ts
@@ -16,12 +16,6 @@ import { CartStatusComponent } from './components/cart-status/cart-status.compon
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './components/login/login.component';
-import { LoginStatusComponent } from './components/login-status/login-status.component';
-
-import { OktaAuthModule, OktaCallbackComponent, OKTA_CONFIG } from '@okta/okta-angular';
-import { OktaAuth } from '@okta/okta-auth-js';
-import myAppConfig from './config/my-app-config';
 
 const oktaConfig = myAppConfig.oidc;
 const oktaAuth = new OktaAuth(oktaConfig);
@@ -50,9 +44,6 @@ const routes: Routes = [
     CartStatusComponent,
     CartDetailsComponent,
     CheckoutComponent,
-    LoginComponent,
-    LoginStatusComponent,
-    
   ],
     
   imports: [
