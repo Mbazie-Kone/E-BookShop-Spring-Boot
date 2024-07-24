879340c2af615c4b956889e1d0929a32b2c75f2a
M'bazie Kone <mbazie89@gmail.com>
2024-07-25 00:31:23 +0200

    Updates

--- front-end/e-bookshop-frontend/src/app/config/my-app-config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    oidc: {
-        clientId: '0oaih5kddeasiJFbq5d7',
-        issuer: 'https://dev-87727243.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
-        scopes: ['openid', 'profile', 'email']
-    }
-}
