30ec9168e7979cdd0179e2cc954287f1c66186c8
M'bazie Kone <mbazie89@gmail.com>
2024-07-25 23:37:33 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/security/Admin.java
+++ src/main/java/it/ecommerce/bookshop/model/security/Admin.java
@@ -20,4 +20,22 @@ public class Admin implements Serializable {
 	
 	private String password;
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
 }
