a7142de9d37df38c455bbe9e349d967b3b1a09de
M'bazie Kone <mbazie89@gmail.com>
2024-07-25 23:54:29 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/repository/AdminRepository.java
+++ src/main/java/it/ecommerce/bookshop/repository/AdminRepository.java
@@ -1,9 +1,11 @@
 package it.ecommerce.bookshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import it.ecommerce.bookshop.model.security.Admin;
 
+@CrossOrigin
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
 }
