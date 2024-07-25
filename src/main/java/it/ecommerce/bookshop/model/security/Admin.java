da2dbb85f334f865782faf040fb1ac732c947406
M'bazie Kone <mbazie89@gmail.com>
2024-07-25 23:34:22 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/security/Admin.java
+++ src/main/java/it/ecommerce/bookshop/model/security/Admin.java
@@ -3,12 +3,16 @@ package it.ecommerce.bookshop.model.security;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Admin implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue
 	private Long id;
 
 }
