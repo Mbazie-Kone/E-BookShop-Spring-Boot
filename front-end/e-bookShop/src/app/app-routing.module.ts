efe44d851006ec41c0a0ef7902160a504f842025
M'bazie Kone <mbazie89@gmail.com>
2024-07-27 02:45:11 +0200

    Updates

--- front-end/e-bookShop/src/app/app-routing.module.ts
+++ front-end/e-bookShop/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProductsComponent } from './components/products/products.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'signup', component: LoginComponent},
-  {path: 'products', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'products', component: ProductsComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'}
 
 ];
