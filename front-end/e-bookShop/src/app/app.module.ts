e7fe8c441a699d9f196e505a71e3e16914e1698d
M'bazie Kone <mbazie89@gmail.com>
2024-07-27 03:48:34 +0200

    Updates

--- front-end/e-bookShop/src/app/app.module.ts
+++ front-end/e-bookShop/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ProductsComponent } from './components/products/products.component';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -19,8 +21,9 @@ import { ProductsComponent } from './components/products/products.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
