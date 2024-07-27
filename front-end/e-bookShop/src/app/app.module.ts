f4b97ce3208e6d9c9ba204309d2b9a4cd7a3ba29
M'bazie Kone <mbazie89@gmail.com>
2024-07-27 02:36:28 +0200

    Updates

--- front-end/e-bookShop/src/app/app.module.ts
+++ front-end/e-bookShop/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
