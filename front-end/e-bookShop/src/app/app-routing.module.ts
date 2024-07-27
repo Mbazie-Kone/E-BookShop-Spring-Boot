736888217a29eb549d64768e484b70e210b9b7c0
M'bazie Kone <mbazie89@gmail.com>
2024-07-27 02:44:18 +0200

    Updates

--- front-end/e-bookShop/src/app/app-routing.module.ts
+++ front-end/e-bookShop/src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: LoginComponent},
   {path: 'products', component: LoginComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'}
+
 ];
 
 @NgModule({
