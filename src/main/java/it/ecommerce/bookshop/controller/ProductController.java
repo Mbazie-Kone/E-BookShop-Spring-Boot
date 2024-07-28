d16a94fe325c06d5dcae160d1a8c720b2b882166
M'bazie Kone <mbazie89@gmail.com>
2024-07-28 22:24:43 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/controller/ProductController.java
+++ src/main/java/it/ecommerce/bookshop/controller/ProductController.java
@@ -1,5 +1,55 @@
 package it.ecommerce.bookshop.controller;
 
-public class ProductController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import it.ecommerce.bookshop.model.Product;
+import it.ecommerce.bookshop.repository.ProductRepository;
+
+public class ProductController {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@GetMapping
+	public List<Product> getAllProducts() {
+		return productRepository.findAll();
+	}
+	
+	// Insert the product in the DB
+	@PostMapping
+	public Product createProduct(@RequestBody Product product) {
+		return productRepository.save(product);
+	}
+	
+	// Update the product in the DB
+	@PostMapping("/{id}")
+	public ResponseEntity<Product> deleteProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+		Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+		product.setSku(productDetails.getSku());
+		product.setName(productDetails.getName());
+		product.setName(productDetails.getName());
+		product.setDescription(productDetails.getDescription());
+		product.setUnitPrice(productDetails.getUnitPrice());
+		product.setImageUrl(productDetails.getImageUrl());
+		product.setActive(productDetails.isActive());
+		product.setUnitsInStock(productDetails.getUnitsInStock());
+		
+		return ResponseEntity.ok(productRepository.save(product));
+	}
+	
+	// Delete the product from the DB
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+		Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+		productRepository.delete(product);
+		return ResponseEntity.noContent().build();
+	}
 }
