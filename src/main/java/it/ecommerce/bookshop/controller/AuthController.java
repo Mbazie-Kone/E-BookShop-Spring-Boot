389c3cca96a21a832b19c2c3a6092a5b6dd4a04a
M'bazie Kone <mbazie89@gmail.com>
2024-07-28 22:28:19 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/controller/AuthController.java
+++ src/main/java/it/ecommerce/bookshop/controller/AuthController.java
@@ -1,9 +1,11 @@
 package it.ecommerce.bookshop.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +18,10 @@ public class AuthController {
 	
 	@Autowired
 	private AuthService authService;
+	
+	
+	public ResponseEntity<?> signup(@RequestBody Map<String, String> user) {
+		
+	}
 	
 }
