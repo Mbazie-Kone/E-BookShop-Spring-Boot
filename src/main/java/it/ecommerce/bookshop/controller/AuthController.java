1e27b70d1a898ec04e55adb25b39fffebb371f5c
M'bazie Kone <mbazie89@gmail.com>
2024-07-28 21:19:55 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/controller/AuthController.java
+++ src/main/java/it/ecommerce/bookshop/controller/AuthController.java
@@ -4,9 +4,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.ecommerce.bookshop.repository.ProductRepository;
+
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
-
+	
+	private ProductRepository productRepository;
+	
 }
