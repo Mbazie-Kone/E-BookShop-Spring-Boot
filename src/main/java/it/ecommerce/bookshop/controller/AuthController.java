5bd3bf6ba72fd33a494a7f0de42d9ad479008dd2
M'bazie Kone <mbazie89@gmail.com>
2024-07-28 21:53:22 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/controller/AuthController.java
+++ src/main/java/it/ecommerce/bookshop/controller/AuthController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,7 @@ public class AuthController {
 		return productRepository.findAll();
 	}
 	
+	@PostMapping
 	public Product createProduct(@RequestBody Product product) {
 		return productRepository.save(product);
 	}
