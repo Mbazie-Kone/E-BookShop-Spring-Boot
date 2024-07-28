7e74ca882f7070b24d5aa9060b3521a5f4ae1a99
M'bazie Kone <mbazie89@gmail.com>
2024-07-28 13:25:41 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/service/impl/AuthServiceImpl.java
+++ src/main/java/it/ecommerce/bookshop/service/impl/AuthServiceImpl.java
@@ -19,14 +19,12 @@ public class AuthServiceImpl implements AuthService {
 	
 	@Override
 	public User signup(String username, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 	@Override
 	public User login(String username, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 }
