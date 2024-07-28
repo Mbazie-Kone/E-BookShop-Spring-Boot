958c0fe49c2da7b26f3feea63d43804fd7fdd182
M'bazie Kone <mbazie89@gmail.com>
2024-07-28 12:49:02 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/model/security/User.java
+++ src/main/java/it/ecommerce/bookshop/model/security/User.java
@@ -49,6 +49,24 @@ public class User implements Serializable {
 		this.role = role;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, password, role, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id) && Objects.equals(password, other.password)
+				&& Objects.equals(role, other.role) && Objects.equals(username, other.username);
+	}
+
 	
 	
 		
