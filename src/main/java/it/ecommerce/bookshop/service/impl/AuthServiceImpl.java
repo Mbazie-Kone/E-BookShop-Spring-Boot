120a6e5e0461f7afb605f93dd5118dda4030dae4
M'bazie Kone <mbazie89@gmail.com>
2024-07-28 13:21:45 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/service/impl/AuthServiceImpl.java
+++ src/main/java/it/ecommerce/bookshop/service/impl/AuthServiceImpl.java
@@ -1,5 +1,25 @@
 package it.ecommerce.bookshop.service.impl;
 
-public class AuthServiceImpl {
+import org.springframework.stereotype.Service;
+
+import it.ecommerce.bookshop.model.security.User;
+import it.ecommerce.bookshop.service.AuthService;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+	
+	
+	
+	@Override
+	public User signup(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User login(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
