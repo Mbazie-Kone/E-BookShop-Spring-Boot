2228d6ad371e048d76ff8e030fb6b59689bcd56d
M'bazie Kone <mbazie89@gmail.com>
2024-07-30 00:34:27 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/model/Product.java
+++ src/main/java/it/mbaziekone/book_e_commerce/model/Product.java
@@ -7,11 +7,7 @@ import java.util.Objects;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import it.ecommerce.bookshop.model.Product;
-import it.ecommerce.bookshop.model.ProductCategory;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
