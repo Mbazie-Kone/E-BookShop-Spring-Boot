047e5c02b96f9796b326f55b3eb58a31b71d7887
M'bazie Kone <mbazie89@gmail.com>
2024-07-29 20:27:41 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/repository/StateRepository.java
+++ src/main/java/it/ecommerce/bookshop/repository/StateRepository.java
@@ -4,13 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import it.ecommerce.bookshop.model.State;
 
-@CrossOrigin("http://localhost:4200")
-@RepositoryRestResource
 public interface StateRepository extends JpaRepository<State, Integer>{
 	
 	public List<State> findByCountryCode(@Param("code") String code);
