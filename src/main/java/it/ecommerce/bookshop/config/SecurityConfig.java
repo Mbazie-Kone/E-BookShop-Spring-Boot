8de7cc133a7c525d556c6d5abb2b18a2d255cacf
M'bazie Kone <mbazie89@gmail.com>
2024-07-29 07:47:36 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/config/SecurityConfig.java
+++ src/main/java/it/ecommerce/bookshop/config/SecurityConfig.java
@@ -4,12 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,17 +19,6 @@ public class SecurityConfig {
 				("/public/**").permitAll().anyRequest().authenticated());
 
 		return http.build();
-	}
-
-	@Bean
-	UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-		UserDetails user = User.builder().username("user").password(passwordEncoder.encode("password")).roles("USER")
-				.build();
-
-		UserDetails admin = User.builder().username("admin").password(passwordEncoder.encode("admin")).roles("ADMIN")
-				.build();
-
-		return new InMemoryUserDetailsManager(user, admin);
 	}
 
 	@Bean
