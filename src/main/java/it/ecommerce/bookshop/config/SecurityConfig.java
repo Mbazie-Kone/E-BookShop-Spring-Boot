ef3c9a605dadb0843245cdce9941b39156be7235
M'bazie Kone <mbazie89@gmail.com>
2024-07-29 05:39:06 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/config/SecurityConfig.java
+++ src/main/java/it/ecommerce/bookshop/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package it.ecommerce.bookshop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,7 +13,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 	
 	
-	public SecurityFilterChain
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+	}
 	
 	@Bean
 	PasswordEncoder passwordEncoder() {
