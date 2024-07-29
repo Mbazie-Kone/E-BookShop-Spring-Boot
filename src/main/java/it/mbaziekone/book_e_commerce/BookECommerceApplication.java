ab0e97ec6ca78153718228b03b4dd4cda6db6e1f
M'bazie Kone <mbazie89@gmail.com>
2024-07-30 00:03:35 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/BookECommerceApplication.java
+++ src/main/java/it/mbaziekone/book_e_commerce/BookECommerceApplication.java
@@ -2,8 +2,10 @@ package it.mbaziekone.book_e_commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "it.mbaziekone.book_e_commerce")
 public class BookECommerceApplication {
 
 	public static void main(String[] args) {
