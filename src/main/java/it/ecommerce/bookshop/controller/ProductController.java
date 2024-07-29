cb945bf3e027f0e9a97b432ed8d07caa448b35e2
M'bazie Kone <mbazie89@gmail.com>
2024-07-29 05:28:12 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/controller/ProductController.java
+++ src/main/java/it/ecommerce/bookshop/controller/ProductController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import it.ecommerce.bookshop.model.Product;
 import it.ecommerce.bookshop.repository.ProductRepository;
 
+@RestController
 public class ProductController {
 	
 	@Autowired
