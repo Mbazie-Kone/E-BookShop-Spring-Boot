2124a80149c9c8f8079075dd5ae80a336be210dd
M'bazie Kone <mbazie89@gmail.com>
2024-07-30 00:46:45 +0200

    Updates

---
+++ src/main/java/it/mbaziekone/book_e_commerce/model/security/User.java
@@ -0,0 +1,10 @@
+package it.mbaziekone.book_e_commerce.model.security;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+}
