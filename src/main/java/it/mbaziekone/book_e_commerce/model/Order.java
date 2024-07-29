d89d3cb3616fd063967b4cd858906daab8470f08
M'bazie Kone <mbazie89@gmail.com>
2024-07-30 01:00:58 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/model/Order.java
+++ src/main/java/it/mbaziekone/book_e_commerce/model/Order.java
@@ -2,6 +2,11 @@ package it.mbaziekone.book_e_commerce.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
