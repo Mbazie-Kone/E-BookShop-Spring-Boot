113fc583bc7d737170da61beccd69b57ba041517
M'bazie Kone <mbazie89@gmail.com>
2024-07-30 00:01:40 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/config/SecurityConfig.java
+++ src/main/java/it/mbaziekone/book_e_commerce/config/SecurityConfig.java
@@ -1,8 +1,22 @@
 package it.mbaziekone.book_e_commerce.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+	
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		/**
+		 * Configuration to permit all the requests
+		 */
+		http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll()).formLogin(Customizer.withDefaults())
+				.httpBasic(Customizer.withDefaults());
 
+		return http.build();
+	}
 }
