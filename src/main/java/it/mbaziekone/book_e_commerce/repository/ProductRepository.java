2688f85fd0c5b6b7cb7637adf36ec89e60a11f64
M'bazie Kone <mbazie89@gmail.com>
2024-07-30 00:40:55 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/repository/ProductRepository.java
+++ src/main/java/it/mbaziekone/book_e_commerce/repository/ProductRepository.java
@@ -1,5 +1,9 @@
 package it.mbaziekone.book_e_commerce.repository;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import it.mbaziekone.book_e_commerce.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
