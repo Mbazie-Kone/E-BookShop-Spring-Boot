3600bbd5be9e6b45b1d7f760b0f869f523222455
M'bazie Kone <mbazie89@gmail.com>
2024-07-30 00:31:19 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/model/ProductCategory.java
+++ src/main/java/it/mbaziekone/book_e_commerce/model/ProductCategory.java
@@ -9,11 +9,15 @@ import it.ecommerce.bookshop.model.Product;
 import it.ecommerce.bookshop.model.ProductCategory;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "product_categories")
 public class ProductCategory {
 	
 private static final long serialVersionUID = 1L;
