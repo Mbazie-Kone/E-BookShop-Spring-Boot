2ae6fcafdb6c5d65d3d26468d26523597b0d4273
M'bazie Kone <mbazie89@gmail.com>
2024-07-29 05:20:51 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/config/SecurityConfig.java
+++ src/main/java/it/ecommerce/bookshop/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package it.ecommerce.bookshop.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -7,8 +8,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig {
 	
-	
-	public PasswordEncoder passwordEncoder() {
+	@Bean
+	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 }
