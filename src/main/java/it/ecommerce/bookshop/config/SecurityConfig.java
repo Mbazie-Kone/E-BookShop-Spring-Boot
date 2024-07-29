0602b454b024d0e819879a1e3878bf8eb5229d85
M'bazie Kone <mbazie89@gmail.com>
2024-07-29 22:18:11 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/config/SecurityConfig.java
+++ src/main/java/it/ecommerce/bookshop/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class AppConfig {
+public class SecurityConfig {
 	
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
