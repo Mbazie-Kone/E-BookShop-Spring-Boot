2bc6294267b45b89eee1ab8d8e2ce61ebbc9e861
M'bazie Kone <mbazie89@gmail.com>
2024-07-29 06:34:03 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/config/DataRestConfig.java
+++ src/main/java/it/ecommerce/bookshop/config/DataRestConfig.java
@@ -1,66 +1,66 @@
-/*
- * package it.ecommerce.bookshop.config;
- * 
- * import java.util.ArrayList; import java.util.List;
- */
-/*
- * import java.util.Set;
- * 
- * import org.springframework.context.annotation.Configuration; import
- * org.springframework.data.rest.core.config.RepositoryRestConfiguration; import
- * org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer; import
- * org.springframework.http.HttpMethod; import
- * org.springframework.web.servlet.config.annotation.CorsRegistry;
- * 
- * import it.ecommerce.bookshop.model.Country; import
- * it.ecommerce.bookshop.model.Product; import
- * it.ecommerce.bookshop.model.ProductCategory; import
- * it.ecommerce.bookshop.model.State; import jakarta.persistence.EntityManager;
- * import jakarta.persistence.metamodel.EntityType;
- * 
- * @Configuration public class DataRestConfig implements
- * RepositoryRestConfigurer {
- * 
- * private EntityManager entityManager;
- * 
- * public DataRestConfig(EntityManager theEntityManager) { entityManager =
- * theEntityManager; }
- * 
- * @Override public void
- * configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
- * CorsRegistry cors) {
- * 
- * HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST,
- * HttpMethod.DELETE};
- * 
- * // disable http methods disableHttpMethods(Product.class, config,
- * theUnsupportedActions); disableHttpMethods(ProductCategory.class, config,
- * theUnsupportedActions); disableHttpMethods(Country.class, config,
- * theUnsupportedActions); disableHttpMethods(State.class, config,
- * theUnsupportedActions);
- * 
- * // call an internal helper method exposeIds(config); }
- * 
- * private void disableHttpMethods(Class theClass, RepositoryRestConfiguration
- * config, HttpMethod[] theUnsupportedActions) { // disable HTTP methods for
- * ProductCategory: PUT, POST and DELETE
- * config.getExposureConfiguration().forDomainType(theClass).withItemExposure((
- * metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
- * .withCollectionExposure((metadata, httpMethods) ->
- * httpMethods.disable(theUnsupportedActions)); }
- * 
- * private void exposeIds(RepositoryRestConfiguration config) {
- * 
- * // get a list of all entity classes from the entity manager
- * Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
- * 
- * // create an array of the entity type List<Class> entityClasses = new
- * ArrayList<>();
- * 
- * // get the entity types for the entities for (EntityType tempEntityType :
- * entities) { entityClasses.add(tempEntityType.getJavaType()); }
- * 
- * // expose the entity id for the array of entity/domain types Class[]
- * domainTypes = entityClasses.toArray(new Class[0]);
- * config.exposeIdsFor(domainTypes); } }
- */
+package it.ecommerce.bookshop.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Set;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import it.ecommerce.bookshop.model.Country;
+import it.ecommerce.bookshop.model.Product;
+import it.ecommerce.bookshop.model.ProductCategory;
+import it.ecommerce.bookshop.model.State;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+	private final EntityManager entityManager;
+
+    public DataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
+
+        // disable HTTP methods for certain entities
+        disableHttpMethods(Product.class, config, theUnsupportedActions);
+        disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+        disableHttpMethods(Country.class, config, theUnsupportedActions);
+        disableHttpMethods(State.class, config, theUnsupportedActions);
+
+        // call an internal helper method
+        exposeIds(config);
+    }
+
+    private void disableHttpMethods(Class<?> theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class<?>> entityClasses = new ArrayList<>();
+
+        // get the entity types for the entities
+        for (EntityType<?> tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // expose the entity ids for the array of entity/domain types
+        Class<?>[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+}
