ba2752c208e7a7f56cea112242489bdf75827849
M'bazie Kone <mbazie89@gmail.com>
2024-07-29 20:26:58 +0200

    Updates

--- src/main/java/it/ecommerce/bookshop/repository/ProductRepository.java
+++ src/main/java/it/ecommerce/bookshop/repository/ProductRepository.java
@@ -4,12 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import it.ecommerce.bookshop.model.Product;
 
 
-@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
 	// behind the scenes, Spring will execute a query similar to this: SELECT * FROM product WHERE category_id = ?
