4ded8fa73e7329853c52a73fc3fdabe19858061b
M'bazie Kone <mbazie89@gmail.com>
2024-07-30 00:47:24 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/model/security/User.java
+++ src/main/java/it/mbaziekone/book_e_commerce/model/security/User.java
@@ -1,10 +1,14 @@
 package it.mbaziekone.book_e_commerce.model.security;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 }
