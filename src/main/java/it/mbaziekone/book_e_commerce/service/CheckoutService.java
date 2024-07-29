70189a558b39105052e0cba03d7110afd683c507
M'bazie Kone <mbazie89@gmail.com>
2024-07-30 01:18:23 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/service/CheckoutService.java
+++ src/main/java/it/mbaziekone/book_e_commerce/service/CheckoutService.java
@@ -1,5 +1,8 @@
 package it.mbaziekone.book_e_commerce.service;
 
+import it.mbaziekone.book_e_commerce.model.dto.Purchase;
+import it.mbaziekone.book_e_commerce.model.dto.PurchaseResponse;
+
 public interface CheckoutService {
 	
 	PurchaseResponse placeOrder(Purchase purchase);
