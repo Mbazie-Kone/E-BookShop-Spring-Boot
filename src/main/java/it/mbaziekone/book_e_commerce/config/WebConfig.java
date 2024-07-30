868115a37370eba165d9a722c86fd29d61b413a1
M'bazie Kone <mbazie89@gmail.com>
2024-07-31 01:58:28 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/config/WebConfig.java
+++ src/main/java/it/mbaziekone/book_e_commerce/config/WebConfig.java
@@ -3,5 +3,7 @@ package it.mbaziekone.book_e_commerce.config;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfig implements WebMvcConfigurer {
+	
+	
 
 }
