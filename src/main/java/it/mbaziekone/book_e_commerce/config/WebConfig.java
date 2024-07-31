a53601cdb2d978ebd5242b546315823c8503a77b
M'bazie Kone <mbazie89@gmail.com>
2024-07-31 07:15:14 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/config/WebConfig.java
+++ src/main/java/it/mbaziekone/book_e_commerce/config/WebConfig.java
@@ -1,10 +1,18 @@
 package it.mbaziekone.book_e_commerce.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 public class WebConfig implements WebMvcConfigurer {
 	
-	ThymeleafViewResolver resolver = new ThymeleafViewResolver()
+	@Bean	
+	ThymeleafViewResolver thymeleafViewResolver() {
+		
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+	}
+	
+	
+	
 
 }
