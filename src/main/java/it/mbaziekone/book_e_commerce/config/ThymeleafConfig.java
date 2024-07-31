781f563aea10bd8456d7a615fb4c8f5c99a73c99
M'bazie Kone <mbazie89@gmail.com>
2024-07-31 08:04:56 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/config/ThymeleafConfig.java
+++ src/main/java/it/mbaziekone/book_e_commerce/config/ThymeleafConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 public class ThymeleafConfig {
@@ -14,6 +15,7 @@ public class ThymeleafConfig {
 		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
 		templateResolver.setPrefix("classpath:/templates/");
 		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode(TemplateMode.HTML);
 	}
 	
 	@Bean	
