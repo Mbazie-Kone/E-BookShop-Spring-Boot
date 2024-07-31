2344f0eb3e3c86960bfe7a4075e25caa63ac5012
M'bazie Kone <mbazie89@gmail.com>
2024-07-31 21:43:20 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/controller/AdminController.java
+++ src/main/java/it/mbaziekone/book_e_commerce/controller/AdminController.java
@@ -4,14 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class AdminController {
 	
 	@GetMapping
-	@ResponseBody
 	public String index() {
 		return "I'm Index";
 	}
