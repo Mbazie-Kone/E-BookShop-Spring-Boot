763e7f03a20660403bc2d9c245fe9323d9a5d5fb
M'bazie Kone <mbazie89@gmail.com>
2024-07-31 23:39:59 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/controller/AdminController.java
+++ src/main/java/it/mbaziekone/book_e_commerce/controller/AdminController.java
@@ -1,7 +1,6 @@
 package it.mbaziekone.book_e_commerce.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,7 +15,7 @@ public class AdminController {
 	}
 	
 	@GetMapping("/products")
-	public String findAllProducts(Model model) {
+	public String findAllProducts() {
 		
 		return "insertForm";
 	}
