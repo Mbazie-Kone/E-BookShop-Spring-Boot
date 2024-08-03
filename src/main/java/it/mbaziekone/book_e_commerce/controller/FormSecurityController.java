65664e8f5efbe08dfda39ed5280f272e9745472a
M'bazie Kone <mbazie89@gmail.com>
2024-08-03 10:52:35 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/controller/FormSecurityController.java
+++ src/main/java/it/mbaziekone/book_e_commerce/controller/FormSecurityController.java
@@ -2,13 +2,11 @@ package it.mbaziekone.book_e_commerce.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
 public class FormSecurityController {
 	
-	@GetMapping("/")
+	@GetMapping("/login")
 	public String loginForm() {
 		
 		return "loginAdmin";
