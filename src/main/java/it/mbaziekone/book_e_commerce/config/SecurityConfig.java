7ca06c29d4232b40552c186922c708cf5a679fe4
M'bazie Kone <mbazie89@gmail.com>
2024-08-03 10:06:09 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/config/SecurityConfig.java
+++ src/main/java/it/mbaziekone/book_e_commerce/config/SecurityConfig.java
@@ -3,18 +3,16 @@ package it.mbaziekone.book_e_commerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
-				.formLogin((login) -> login.loginPage("/loginAdmin").loginProcessingUrl("/perform_login")
-						.defaultSuccessUrl("/", true).failureUrl("/login?error=true"));
+		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated()
+				).formLogin((login) -> login.loginPage("/loginAdmin").loginProcessingUrl("/perform_login")
+						.defaultSuccessUrl("/admin", true).failureUrl("/login?error=true"));
 
 		return http.build();
 	}
