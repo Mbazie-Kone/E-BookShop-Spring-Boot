d67b1d81dda23af3b485d1a1656efb1a26b3561b
M'bazie Kone <mbazie89@gmail.com>
2024-08-04 12:49:18 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/config/SecurityConfig.java
+++ src/main/java/it/mbaziekone/book_e_commerce/config/SecurityConfig.java
@@ -3,45 +3,18 @@ package it.mbaziekone.book_e_commerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorize -> authorize
-				.requestMatchers("/public/**", "/login", "/error").permitAll()
-				.anyRequest().authenticated())
-				.formLogin(login -> login.loginPage("/login").loginProcessingUrl("/perform_login")
-						.defaultSuccessUrl("/", true).failureUrl("/login?error=true"))
-				.logout(logout -> logout.logoutUrl("/perform_logout").deleteCookies("JSESSIONID")
-						.logoutSuccessUrl("/login?logout=true"));
 
 		return http.build();
 	}
 
-	/*
-	 * @Bean SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
-	 * throws Exception {
-	 *//**
-		 * Configuration to permit all the requests
-		 *//*
-			 * http.authorizeHttpRequests(requests ->
-			 * requests.anyRequest().permitAll()).formLogin(Customizer.withDefaults())
-			 * .httpBasic(Customizer.withDefaults());
-			 * 
-			 * return http.build(); }
-			 */
-
-	/*
-	 * @Bean SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
-	 * throws Exception {
-	 * 
-	 * http.authorizeHttpRequests(( requests ) -> requests
-	 * .anyRequest().authenticated() ).formLogin(Customizer.withDefaults())
-	 * .httpBasic(Customizer.withDefaults());
-	 * 
-	 * return http.build(); }
-	 */
+	
 }
