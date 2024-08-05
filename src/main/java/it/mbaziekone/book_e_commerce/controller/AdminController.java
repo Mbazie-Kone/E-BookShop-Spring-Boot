dba1f88fee0a5a604ce7a7257315a7a29ed7b04e
M'bazie Kone <mbazie89@gmail.com>
2024-08-05 06:16:24 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/controller/AdminController.java
+++ src/main/java/it/mbaziekone/book_e_commerce/controller/AdminController.java
@@ -1,8 +1,13 @@
 package it.mbaziekone.book_e_commerce.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class AdminController {
@@ -20,6 +25,12 @@ public class AdminController {
 	}
 	
 	@PostMapping("/perform_logout")
-	public String logout
+	public String logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+		if(authentication != null) {
+			new SecurityContextLogoutHandler().logout(request, response, authentication);	
+		}
+		
+		return "redirect:/login?logout=true";
+	}
 	
 }
