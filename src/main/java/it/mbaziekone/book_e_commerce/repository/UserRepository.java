ae40526f48bdfca692c2523e35d991bcc4abcbd0
M'bazie Kone <mbazie89@gmail.com>
2024-08-16 02:21:49 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package it.mbaziekone.book_e_commerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.mbaziekone.book_e_commerce.model.security.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
