cbea3cc4ec0ebca6c72c38c2c96cc21c261573c0
M'bazie Kone <mbazie89@gmail.com>
2024-08-16 03:03:12 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/controller/ProductController.java
+++ src/main/java/it/mbaziekone/book_e_commerce/controller/ProductController.java
@@ -1,10 +1,16 @@
 package it.mbaziekone.book_e_commerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.mbaziekone.book_e_commerce.service.ProductService;
 
 @Controller
 @RequestMapping("/admin/products")
 public class ProductController {
+	
+	@Autowired
+	private ProductService productService;
 
 }
