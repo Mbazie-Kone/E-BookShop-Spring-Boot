e4ab138b2ad5874be3038b577a0bd8355583b7df
M'bazie Kone <mbazie89@gmail.com>
2024-08-16 03:19:46 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/controller/ProductController.java
+++ src/main/java/it/mbaziekone/book_e_commerce/controller/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.mbaziekone.book_e_commerce.model.Product;
@@ -27,5 +28,11 @@ public class ProductController {
 		Product product = new Product();
 		model.addAttribute("product", product);
 		return "admin/new_product";
+	}
+	
+	
+	
+	public String saveProduct(@ModelAttribute("product") Product product) {
+		
 	}
 }
