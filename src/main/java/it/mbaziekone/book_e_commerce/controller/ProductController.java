f43320bac1276391d8ea009f0ea2fd140b5edc28
M'bazie Kone <mbazie89@gmail.com>
2024-08-16 03:15:58 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/controller/ProductController.java
+++ src/main/java/it/mbaziekone/book_e_commerce/controller/ProductController.java
@@ -3,6 +3,7 @@ package it.mbaziekone.book_e_commerce.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.mbaziekone.book_e_commerce.service.ProductService;
@@ -14,9 +15,14 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
+	@GetMapping
 	public String viewProductsPage(Model model) {
 		model.addAttribute("listProducts", productService.getAllCatalogs());
 		return "admin/products"; //This is the view that contains the dashboard layout
 	}
-
+	
+	@GetMapping("/showNewCatalogForm")
+	public String showProductForm(Model model) {
+		
+	}
 }
