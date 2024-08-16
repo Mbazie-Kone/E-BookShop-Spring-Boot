23d6f77652aeed4a9ee12e9db6ecf584658b7053
M'bazie Kone <mbazie89@gmail.com>
2024-08-16 02:38:03 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/service/impl/ProductServiceImpl.java
+++ src/main/java/it/mbaziekone/book_e_commerce/service/impl/ProductServiceImpl.java
@@ -1,12 +1,15 @@
 package it.mbaziekone.book_e_commerce.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.mbaziekone.book_e_commerce.repository.ProductRepository;
 import it.mbaziekone.book_e_commerce.service.ProductService;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 	
-	
+	@Autowired
+	private ProductRepository productRepository;
 
 }
