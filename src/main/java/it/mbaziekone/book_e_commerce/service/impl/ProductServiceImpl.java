10ae2e974a8dcc78878f596993ab35a93d78263b
M'bazie Kone <mbazie89@gmail.com>
2024-08-16 02:36:40 +0200

    Updates

---
+++ src/main/java/it/mbaziekone/book_e_commerce/service/impl/ProductServiceImpl.java
@@ -0,0 +1,12 @@
+package it.mbaziekone.book_e_commerce.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import it.mbaziekone.book_e_commerce.service.ProductService;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+	
+	
+
+}
