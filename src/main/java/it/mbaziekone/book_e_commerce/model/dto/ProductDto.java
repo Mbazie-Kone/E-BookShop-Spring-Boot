948c3b49a11728f345a12da44169062b2489d763
M'bazie Kone <mbazie89@gmail.com>
2024-08-18 01:37:56 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/model/dto/ProductDto.java
+++ src/main/java/it/mbaziekone/book_e_commerce/model/dto/ProductDto.java
@@ -2,6 +2,8 @@ package it.mbaziekone.book_e_commerce.model.dto;
 
 import java.math.BigDecimal;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class ProductDto {
 	
 	private String sku;
@@ -11,4 +13,6 @@ public class ProductDto {
 	private String description;
 	
 	private BigDecimal unitPrice;
+	
+	private MultipartFile imageFilename;
 }
