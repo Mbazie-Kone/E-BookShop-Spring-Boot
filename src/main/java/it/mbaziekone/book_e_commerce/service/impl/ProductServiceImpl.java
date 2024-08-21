31db5beb244ed5c12da96ec3fcce0a501de52f0c
M'bazie Kone <mbazie89@gmail.com>
2024-08-21 06:19:38 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/service/impl/ProductServiceImpl.java
+++ src/main/java/it/mbaziekone/book_e_commerce/service/impl/ProductServiceImpl.java
@@ -48,7 +48,7 @@ public class ProductServiceImpl implements ProductService {
 		productTwo.setName(product.getName());
 		productTwo.setDescription(product.getDescription());
 		productTwo.setUnitPrice(product.getUnitPrice());
-		product.setImagePath("images/" + uniqueFileName);
+		//product.setImagePath("images/" + uniqueFileName);
 		productTwo.setActive(product.isActive());
 		productTwo.setUnitsInStock(product.getUnitsInStock());
 		productTwo.setDateCreated(product.getDateCreated());
@@ -74,6 +74,6 @@ public class ProductServiceImpl implements ProductService {
 			e.printStackTrace();
 		}
 		
-		productRepository.deleteById(id);	
+		productRepository.deleteById(id);
 	}
 }
