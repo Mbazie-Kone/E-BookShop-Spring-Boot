0aad6e8f2b4e254d5d15d7deb15f073cd6d3025a
M'bazie Kone <mbazie89@gmail.com>
2024-08-22 13:11:54 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/service/impl/CustomUserDetailsService.java
+++ src/main/java/it/mbaziekone/book_e_commerce/service/impl/CustomUserDetailsService.java
@@ -1,30 +1,18 @@
 package it.mbaziekone.book_e_commerce.service.impl;
 
-import java.util.Collection;
 
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class CustomUserDetailsService implements UserDetails {
-
-	private static final long serialVersionUID = 1L;
+public class CustomUserDetailsService implements UserDetailsService {
 
 	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
