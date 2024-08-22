caf566725e90f72a3e27c1fa790c661792eb2eca
M'bazie Kone <mbazie89@gmail.com>
2024-08-22 12:22:56 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/model/security/Admin.java
+++ src/main/java/it/mbaziekone/book_e_commerce/model/security/Admin.java
@@ -1,8 +1,10 @@
 package it.mbaziekone.book_e_commerce.model.security;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "administrators")
 public class Admin {
 
 }
