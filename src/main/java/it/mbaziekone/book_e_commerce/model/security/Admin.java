6b17228ba6cc6cc3480ac6f1e5a9c64f5cd65a5f
M'bazie Kone <mbazie89@gmail.com>
2024-08-22 12:24:00 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/model/security/Admin.java
+++ src/main/java/it/mbaziekone/book_e_commerce/model/security/Admin.java
@@ -1,10 +1,17 @@
 package it.mbaziekone.book_e_commerce.model.security;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "administrators")
 public class Admin {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 }
