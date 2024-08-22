91bfa4a3f3daf80dbc05cae682c49669670db74e
M'bazie Kone <mbazie89@gmail.com>
2024-08-22 13:14:11 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/service/impl/CustomUserDetailsService.java
+++ src/main/java/it/mbaziekone/book_e_commerce/service/impl/CustomUserDetailsService.java
@@ -1,15 +1,19 @@
 package it.mbaziekone.book_e_commerce.service.impl;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import it.mbaziekone.book_e_commerce.repository.AdminRepository;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 	
-	
+	@Autowired
+	AdminRepository adminRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
