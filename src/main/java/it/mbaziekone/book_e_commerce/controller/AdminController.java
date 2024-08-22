5c5952f46c38018265ee702750658b8b77aa3fa8
M'bazie Kone <mbazie89@gmail.com>
2024-08-22 14:29:44 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/controller/AdminController.java
+++ src/main/java/it/mbaziekone/book_e_commerce/controller/AdminController.java
@@ -1,16 +1,25 @@
 package it.mbaziekone.book_e_commerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import it.mbaziekone.book_e_commerce.repository.AdminRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class AdminController {
+	
+	@Autowired
+	private AdminRepository adminRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@GetMapping("/loginAdmin")
 	public String login() {
