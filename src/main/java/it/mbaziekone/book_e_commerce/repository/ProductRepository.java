c69ae986b6efba27d43bceba33f9af6a6842091e
M'bazie Kone <mbazie89@gmail.com>
2024-08-29 06:07:24 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/repository/ProductRepository.java
+++ src/main/java/it/mbaziekone/book_e_commerce/repository/ProductRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import it.mbaziekone.book_e_commerce.model.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
 	// behind the scenes, Spring will execute a query similar to this: SELECT * FROM product WHERE category_id = ?
