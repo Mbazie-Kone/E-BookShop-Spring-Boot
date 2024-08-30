9f7eb35b4eba9bba228ef65f6acf6a869fe62a03
M'bazie Kone <mbazie89@gmail.com>
2024-08-30 03:53:12 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/config/SecurityConfig.java
+++ src/main/java/it/mbaziekone/book_e_commerce/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
 
 		return http
 				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/public/**", "/login", "/register", "/error","/api/**").permitAll()
+						.requestMatchers("/public/**","/register", "/error","/api/**").permitAll()
 						.anyRequest().authenticated())
 				.formLogin(form -> form
 						.loginPage("/loginAdmin")
