78aaea8389b6c66609ded68e6debfda74c7789ad
M'bazie Kone <mbazie89@gmail.com>
2024-08-30 04:28:27 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/config/DataRestConfig.java
+++ src/main/java/it/mbaziekone/book_e_commerce/config/DataRestConfig.java
@@ -1,5 +1,9 @@
 package it.mbaziekone.book_e_commerce.config;
 
-public class DataRestConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
 
 }
