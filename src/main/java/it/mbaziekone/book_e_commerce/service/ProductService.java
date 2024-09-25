acf37aa9ab5bad15150184136f8d7ca83f311c26
M'bazie Kone <mbazie89@gmail.com>
2024-09-26 00:42:45 +0200

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/service/ProductService.java
+++ src/main/java/it/mbaziekone/book_e_commerce/service/ProductService.java
@@ -3,6 +3,7 @@ package it.mbaziekone.book_e_commerce.service;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 import it.mbaziekone.book_e_commerce.model.Product;
@@ -16,5 +17,7 @@ public interface ProductService {
 	public Product getProductById(Long id);
 	
 	public void deleteProduct(Long id);
+	
+	public Page<Product> getPaginatedProducts(int page, int size);
 	
 }
