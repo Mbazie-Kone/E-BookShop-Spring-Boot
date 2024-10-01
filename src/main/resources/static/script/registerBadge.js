179339a82fb7c5968eeeb021ed0f74936fe15476
M'bazie Kone <mbazie89@gmail.com>
2024-10-01 20:39:45 +0200

    Updates

--- src/main/resources/static/script/registerBadge.js
+++ src/main/resources/static/script/registerBadge.js
@@ -1,3 +1,8 @@
-/**
- * 
- */
+document.addEventListener("DOMContentLoaded", function() {
+	
+	let usernameErrorDiv = document.getElementById("usernameError");
+	if(usernameErrorDiv && !usernameErrorDiv.classList.contains("d-none")) {
+		showDialog(usernameErrorDiv, "Error in Username field")
+	}
+	
+})
