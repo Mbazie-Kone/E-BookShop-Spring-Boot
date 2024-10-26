df8b511b0d391bb1641a50cc370b2a17d22d483f
M'bazie Kone <mbazie89@gmail.com>
2024-10-26 18:50:57 +0200

    Updates

--- src/main/resources/static/javascript/javaScripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-	const modalBtn = document.getElementById('modalBtn');
-	const registrationModal = new bootstrap.Modal(document.getElementById('registrationModal'));
-	
-	modalBtn.addEventListener('click', function(){
-		registrationModal.show();
-	});
-});
