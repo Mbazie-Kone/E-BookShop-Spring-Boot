d256f57a5ed267376ca5f4e6657d3871aebc240d
M'bazie Kone <mbazie89@gmail.com>
2024-11-02 12:58:01 +0100

    Updates

---
+++ src/main/java/it/mbaziekone/book_e_commerce/service/AdminService.java
@@ -0,0 +1,8 @@
+package it.mbaziekone.book_e_commerce.service;
+
+import it.mbaziekone.book_e_commerce.model.security.Admin;
+
+public interface AdminService {
+	
+	public void saveAdmin(Admin admin);
+}
---
+++ src/main/java/it/mbaziekone/book_e_commerce/service/impl/AdminServiceImpl.java
@@ -0,0 +1,20 @@
+package it.mbaziekone.book_e_commerce.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.mbaziekone.book_e_commerce.model.security.Admin;
+import it.mbaziekone.book_e_commerce.repository.AdminRepository;
+import it.mbaziekone.book_e_commerce.service.AdminService;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+	
+	@Autowired
+	private AdminRepository adminRepository;
+
+	@Override
+	public void saveAdmin(Admin admin) {	
+		adminRepository.save(admin);
+	}
+}
