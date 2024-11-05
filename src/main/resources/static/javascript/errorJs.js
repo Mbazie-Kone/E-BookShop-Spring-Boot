fc59a09f6134248be66db802164baddc93fb3da8
M'bazie Kone <mbazie89@gmail.com>
2024-11-06 00:57:28 +0100

    Updates

--- src/main/resources/static/javascript/errorJs.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    if ([[${showRegistrationModal}]] == true) {
-        new bootstrap.Modal(document.getElementById('registrationModal')).show();
-    }
-});
