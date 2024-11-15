4294638a5bdfe582a6a3c06c693f2f1daa0251f0
M'bazie Kone <mbazie89@gmail.com>
2024-11-15 19:15:08 +0100

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/model/dto/AdminDto.java
+++ src/main/java/it/mbaziekone/book_e_commerce/model/dto/AdminDto.java
@@ -1,6 +1,7 @@
 package it.mbaziekone.book_e_commerce.model.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 public class AdminDto {
 	
@@ -8,6 +9,7 @@ public class AdminDto {
 	private String username;
 	
 	@NotEmpty(message = "Password is required")
+	@Size(min = 12, message = "Password must be at least 12 caracters long")
 	private String password;
 	
 	private String role;
