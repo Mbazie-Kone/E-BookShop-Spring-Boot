b59066bfcee5ce4d803219b0de497f30407bef1d
M'bazie Kone <mbazie89@gmail.com>
2024-11-15 19:12:51 +0100

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/model/dto/AdminDto.java
+++ src/main/java/it/mbaziekone/book_e_commerce/model/dto/AdminDto.java
@@ -1,8 +1,14 @@
 package it.mbaziekone.book_e_commerce.model.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class AdminDto {
 	
+	@NotEmpty(message = "Username is required")
 	private String username;
 	
+	@NotEmpty(message = "Password is required")
 	private String password;
+	
+	private String role;
 }
