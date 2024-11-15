08e1e53adcb2a8dc19b0a2495ca588d2c9c755ab
M'bazie Kone <mbazie89@gmail.com>
2024-11-15 19:27:07 +0100

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/service/impl/AdminServiceImpl.java
+++ src/main/java/it/mbaziekone/book_e_commerce/service/impl/AdminServiceImpl.java
@@ -1,8 +1,10 @@
 package it.mbaziekone.book_e_commerce.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import it.mbaziekone.book_e_commerce.model.dto.AdminDto;
 import it.mbaziekone.book_e_commerce.model.security.Admin;
 import it.mbaziekone.book_e_commerce.repository.AdminRepository;
 import it.mbaziekone.book_e_commerce.service.AdminService;
@@ -12,15 +14,21 @@ public class AdminServiceImpl implements AdminService {
 	
 	@Autowired
 	private AdminRepository adminRepository;
+	
+	private final PasswordEncoder passwordEncoder = null;
 
 	@Override
-	public void saveAdmin(Admin admin) {	
+	public void saveAdmin(AdminDto adminDto) {
+		Admin admin	= new Admin();
+		admin.setUsername(adminDto.getUsername());
+		admin.setPassword(passwordEncoder.encode(adminDto.getPassword()));
 		adminRepository.save(admin);
 	}
-
+	
 	@Override
 	public Admin findByUsername(String username) {
 		
 		return adminRepository.findByUsername(username);
 	}
+
 }
