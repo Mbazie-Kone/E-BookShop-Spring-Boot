9745c5b74e29364c86aa9070c3417c1f34a964ad
M'bazie Kone <mbazie89@gmail.com>
2024-11-16 20:46:38 +0100

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/model/dto/AdminDto.java
+++ src/main/java/it/mbaziekone/book_e_commerce/model/dto/AdminDto.java
@@ -1,5 +1,6 @@
 package it.mbaziekone.book_e_commerce.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
@@ -8,12 +9,12 @@ public class AdminDto {
 	@NotEmpty(message = "Username is required")
 	private String username;
 	
-	@NotEmpty(message = "Password is required")
-	@Size(min = 12, message = "Password must be at least 12 caracters long")
+	@NotBlank(message = "The password cannot be empty")
+	@Size(min = 8, message = "Password must be at least 8 caracters long")
 	private String password;
 	
-	@NotEmpty(message = "Confirm password is required")
-	@Size(min = 12, message = "Confirm password must be at least 12 caracters long")
+	@NotBlank(message = "The confirm password cannot be empty")
+	@Size(min = 8, message = "Confirm password must be at least 8 caracters long")
 	private String confirmPassword;
 
 	public String getUsername() {
