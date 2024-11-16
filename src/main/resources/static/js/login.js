ac3754921f8e8ae2c26418ab3026176ade548f39
M'bazie Kone <mbazie89@gmail.com>
2024-11-16 18:45:29 +0100

    Updates

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 	const showRegisterModal = document.getElementById("registerModal").getAttribute("data-show");
+	if(showRegisterModal === "true") {
+		const modal = new bootstrap.Modal(document.getElementById("registerModal"));
+		modal.show();
+	}
 })
