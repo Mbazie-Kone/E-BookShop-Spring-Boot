3a20b3023a9b8ca1fd08531b1bbfd80c60ac3272
M'bazie Kone <mbazie89@gmail.com>
2024-11-16 14:23:57 +0100

    Updates

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("signUpForm");
     const inputs = form.querySelectorAll("input");
+	let formSubmitted = false; // Indicates whether the form has been submitted at least once
 
     inputs.forEach(input => {
         input.addEventListener("input", function () {
