c7830f15ef7fd3696ee6752c114bf785d63a4b78
M'bazie Kone <mbazie89@gmail.com>
2024-11-18 04:31:38 +0100

    Updates

--- src/main/resources/static/js/dashboardCharts.js
+++ src/main/resources/static/js/dashboardCharts.js
@@ -1,8 +1,9 @@
-const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
-const yArray = [55, 49, 44, 24, 15];
+document.addEventListener("DOMContentLoaded", function () {
+    const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
+    const yArray = [55, 49, 44, 24, 15];
+    const layout = { title: "E-Book Shop KMB/>" };
+    const data = [{ labels: xArray, values: yArray, hole: 0.4, type: "pie" }];
+    Plotly.newPlot("myPlot", data, layout);
+});
 
-const layout = {title:"E-Book Shop KMB/>"};
-
-const data = [{labels:xArray, values:yArray, hole:.4, type:"pie"}];
-
-Plotly.newPlot("myPlot", data, layout);
+console.log(document.getElementById("myPlot"));
