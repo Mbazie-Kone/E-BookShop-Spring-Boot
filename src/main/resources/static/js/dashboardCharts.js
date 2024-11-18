308ba8dce6734d5be4b8c7681743913d78daa279
M'bazie Kone <mbazie89@gmail.com>
2024-11-18 04:16:11 +0100

    Updates

--- src/main/resources/static/js/dashboardCharts.js
+++ src/main/resources/static/js/dashboardCharts.js
@@ -1,26 +1,8 @@
-const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-const yValues = [55, 49, 44, 24, 15];
-const barColors = [
-  "#b91d47",
-  "#00aba9",
-  "#2b5797",
-  "#e8c3b9",
-  "#1e7145"
-];
+const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
+const yArray = [55, 49, 44, 24, 15];
 
-new Chart("demoChart", {
-  type: "pie",
-  data: {
-    labels: xValues,
-    datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }]
-  },
-  options: {
-    title: {
-      display: true,
-      text: "E-book 2024 KMB"
-    }
-  }
-});
+const layout = {title:"E-Book Shop KMB/>"};
+
+const data = [{labels:xArray, values:yArray, hole:.4, type:"pie"}];
+
+Plotly.newPlot("myPlot", data, layout);
