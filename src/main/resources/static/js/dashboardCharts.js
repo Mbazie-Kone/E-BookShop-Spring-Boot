d6dc804f1e0112337669e8f8e2cf457159a90d4f
M'bazie Kone <mbazie89@gmail.com>
2024-11-18 19:54:03 +0100

    Updates

--- src/main/resources/static/js/dashboardCharts.js
+++ src/main/resources/static/js/dashboardCharts.js
@@ -1,9 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
-    const yArray = [55, 49, 44, 24, 15];
-    const layout = { title: "E-Book Shop KMB/>" };
-    const data = [{ labels: xArray, values: yArray, hole: 0.4, type: "pie" }];
-    Plotly.newPlot("myPlot", data, layout);
-});
-
-console.log(document.getElementById("myPlot"));
+// Function to retrieve data from the backend
+async function fetchDonutChartData() {
+	const response = await fetch('api/chart/donut-data');
+	return response.json();
+}
