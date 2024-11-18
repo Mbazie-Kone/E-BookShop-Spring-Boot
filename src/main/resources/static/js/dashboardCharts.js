c3a5049ea7cd0d8be363b1120e9a2b0862cb296f
M'bazie Kone <mbazie89@gmail.com>
2024-11-18 19:56:44 +0100

    Updates

--- src/main/resources/static/js/dashboardCharts.js
+++ src/main/resources/static/js/dashboardCharts.js
@@ -3,3 +3,8 @@ async function fetchDonutChartData() {
 	const response = await fetch('api/chart/donut-data');
 	return response.json();
 }
+
+//  Function to initialize the Donut Chart
+async function initializeDonutChart() {
+	const data = await fetchDonutChartData();
+}
