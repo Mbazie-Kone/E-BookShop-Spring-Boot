4a17a96dffba37b359bac89deadbcd1e9584b96d
M'bazie Kone <mbazie89@gmail.com>
2024-11-18 19:01:09 +0100

    Updates

--- src/main/java/it/mbaziekone/book_e_commerce/controller/rest/ChartRestController.java
+++ src/main/java/it/mbaziekone/book_e_commerce/controller/rest/ChartRestController.java
@@ -3,9 +3,15 @@ package it.mbaziekone.book_e_commerce.controller.rest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.mbaziekone.book_e_commerce.repository.ProductRepository;
+
 @RestController
 @RequestMapping("/api/chart")
 public class ChartRestController {
 	
+	private ProductRepository productRepository;
 	
+	public ChartRestController(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
 }
