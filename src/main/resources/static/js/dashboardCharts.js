07153a9ff59f14d43acbc2f08c44ca4020e63e12
M'bazie Kone <mbazie89@gmail.com>
2024-11-18 20:03:20 +0100

    Updates

--- src/main/resources/static/js/dashboardCharts.js
+++ src/main/resources/static/js/dashboardCharts.js
@@ -7,4 +7,21 @@ async function fetchDonutChartData() {
 //  Function to initialize the Donut Chart
 async function initializeDonutChart() {
 	const data = await fetchDonutChartData();
+	const ctx = document.getElementById('donutChart').getContext('2d');
+	new Chart(ctx, {
+		type: 'doughnut',
+		data: {
+			labels: data.labels,
+			datasets: [{
+				label: 'Distribution of Products by Category',
+				data: data.values,
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.5)',
+					'rgba(54, 162, 235, 0.5)',
+					'rgba(255, 206, 86, 0.5)',
+					'rgba(75, 192, 192, 0.5)'
+				],
+			}]
+		}
+	})
 }
