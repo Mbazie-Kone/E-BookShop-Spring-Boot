413ddfa4ab5bf2ce642cba7777477563171a85fb
M'bazie Kone <mbazie89@gmail.com>
2024-11-19 23:40:34 +0100

    Updates

--- src/main/resources/static/js/dashboardCharts.js
+++ src/main/resources/static/js/dashboardCharts.js
@@ -79,8 +79,30 @@ async function initializaAvailabilityChart() {
 				backgroundColor: [
 					'rgba(75, 192, 192, 0.5)', // Color for "Available"
 					'rgba(255, 99, 132, 0.5)'  // Color for "Not Available"
-				]
+				],
+				borderColor: [
+					'rgba(75, 192, 192, 1)',
+					'rgba(255, 99, 132, 1)'
+				],
+				borderWidth: 1
 			}]
+		},
+		options: {
+			responsive: true,
+			plugins: {
+				legend: {
+					position: 'top',
+				},
+				tooltip: {
+					callbacks: {
+						label: function(tooltipItem) {
+							const total = data.values.reduce((acc, val) => acc + val, 0);
+							const value = data.values[tooltipItem.dataIndex];
+							const percentage = ((value / total) * 100).toFixed(2);
+						}
+					}
+				}
+			}
 		}
 	})
 }
